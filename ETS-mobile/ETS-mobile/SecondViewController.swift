0ca6755daadf08fe50fad5ae632cd5120286ef56
Xavier <xavier@ApplETSs-iMac.local>
2018-03-10 13:37:31 -0500

    Initial Commit

---
+++ ETS-mobile/ETS-mobile/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ETS-mobile
+//
+//  Created by Xavier on 3/10/18.
+//  Copyright © 2018 applETS. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
