d29f043549630f05aefe58d422985fd4cbcba4a6
Thanh-Son-Philippe Lam <thanh-son-philippe.lam.1@ens.etsmtl.ca>
2018-05-06 10:28:37 -0400

    Add listeCours and comments to SignetsApi

--- app/src/main/kotlin/ca/etsmtl/etsmobile/data/api/SignetsApi.kt
+++ app/src/main/kotlin/ca/etsmtl/etsmobile/data/api/SignetsApi.kt
@@ -18,14 +18,31 @@ import retrofit2.http.POST
 import javax.inject.Singleton
 
 /**
+ * This interface is implemented by Retrofit. It's used to fetch data from Signets web service.
+ * https://signets-ens.etsmtl.ca/Secure/WebServices/SignetsMobile.asmx
+ *
  * Created by Sonphil on 02-03-18.
  */
 @Singleton
 interface SignetsApi {
+    /**
+     * Fetches a list of the student's programs
+     *
+     * @param body The student's credentials
+     * @return A List of the student's programs
+     */
     @Headers("Accept: application/json", "Content-Type: application/json", "Accept-Charset: UTF-8")
     @POST("listeProgrammes")
     fun listeProgrammes(@Body body: SignetsUserCredentials): LiveData<ApiResponse<SignetsModel<ListeProgrammes>>>
 
+    /**
+     * Fetches the schedules of the student's final exams with room locations
+     *
+     * @param codeAccesUniversel The student's universal code
+     * @param motPasse The student's password
+     * @param pSession The session
+     * @return The schedules of the student's final exams with room locations
+     */
     @Headers("Accept: application/json", "Content-Type: application/json", "Accept-Charset: UTF-8")
     @FormUrlEncoded
     @POST("listeHoraireExamensFin")
@@ -35,6 +52,15 @@ interface SignetsApi {
         @Field("pSession") pSession: String
     ): LiveData<ApiResponse<SignetsModel<ListeHoraireExamensFinaux>>>
 
+    /**
+     * Fetches a list of courses of the student between two sessions
+     *
+     * @param codeAccesUniversel The student's universal code
+     * @param motPasse The student's password
+     * @param sesDebut The interval starting session
+     * @param sesFin The interval ending session
+     * @return A list of courses of the student between the specified sessions
+     */
     @Headers("Accept: application/json", "Content-Type: application/json", "Accept-Charset: UTF-8")
     @FormUrlEncoded
     @POST("listeHoraireExamensFin")
@@ -45,6 +71,16 @@ interface SignetsApi {
         @Field("SesFin") sesFin: String
     ): LiveData<ApiResponse<SignetsModel<ListeDeCours>>>
 
+    /**
+     * Fetches a list of the student's activities (courses, labs, etc.) with their schedule and
+     * local, as well as the teachers
+     *
+     * @param codeAccesUniversel The student's universal code
+     * @param motPasse The student's password
+     * @param pSession The session
+     * @return A list of the student's activities (courses, labs, etc.) with their schedule and
+     * local, as well as the teachers
+     */
     @Headers("Accept: application/json", "Content-Type: application/json", "Accept-Charset: UTF-8")
     @FormUrlEncoded
     @POST("listeHoraireEtProf")
@@ -54,15 +90,43 @@ interface SignetsApi {
         @Field("pSession") pSession: String
     ): LiveData<ApiResponse<SignetsModel<ListeDesActivitesEtProf>>>
 
+    /**
+     * Fetches basic information about the student: name, first name, permanent code and balance
+     *
+     * @param body The student's credentials
+     * @return Basic information about the student
+     */
     @Headers("Accept: application/json", "Content-Type: application/json", "Accept-Charset: UTF-8")
     @POST("infoEtudiant")
     fun infoEtudiant(@Body body: SignetsUserCredentials): LiveData<ApiResponse<SignetsModel<Etudiant>>>
 
+    /**
+     * Fetches a list of days that replace others
+     *
+     * @param pSession The session
+     * @return A list of days that replace others for the specified session
+     */
     @Headers("Accept: application/json", "Content-Type: application/json", "Accept-Charset: UTF-8")
     @FormUrlEncoded
     @POST("lireJoursRemplaces")
     fun listeJoursRemplaces(@Field("pSession") pSession: String): LiveData<ApiResponse<SignetsModel<ListeJoursRemplaces>>>
 
+    /**
+     * Fetches a list of all courses of the student. The is list is sorted by session and acronym.
+     *
+     * @param body The student's credentials
+     * @return A list of all courses of the student
+     */
+    @Headers("Accept: application/json", "Content-Type: application/json", "Accept-Charset: UTF-8")
+    @POST("listeCours")
+    fun listeCours(@Body body: SignetsUserCredentials): LiveData<ApiResponse<SignetsModel<ListeDeCours>>>
+
+    /**
+     * Fetches a list of sessions of the student
+     *
+     * @param body The student's credentials
+     * @return A list of sessions of the student
+     */
     @Headers("Accept: application/json", "Content-Type: application/json", "Accept-Charset: UTF-8")
     @POST("listeSessions")
     fun listeSessions(@Body body: SignetsUserCredentials): LiveData<ApiResponse<SignetsModel<ListeDeSessions>>>
