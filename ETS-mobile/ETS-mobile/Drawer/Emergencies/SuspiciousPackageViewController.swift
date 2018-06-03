40eb9c52a4a2963909dff5730b6e5eec5d60c3cd
Emmanuel Proulx <foufoulefou2juillet@hotmail.com>
2018-06-03 16:28:21 -0400

    Suspicious Package View
    - Static view with behaviour instructions
    -Translated to English and French
    -Also made corrections to variable names in the bomb threat view controller.

--- ETS-mobile/ETS-mobile/Drawer/Emergencies/SuspiciousPackageViewController.swift
+++ ETS-mobile/ETS-mobile/Drawer/Emergencies/SuspiciousPackageViewController.swift
@@ -1,17 +1,19 @@
 //
-//  BombeViewController.swift
+//  SuspiciousPackageViewController.swift
 //  ETS-mobile
 //
-//  Created by Emmanuel on 2018-04-21.
+//  Created by Emmanuel on 2018-06-03.
 //  Copyright © 2018 applETS. All rights reserved.
 //
 
 import UIKit
 
-class BombThreatViewController: UIViewController {
+class SuspiciousPackageViewController: UIViewController {
+
+    @IBOutlet weak var emergencyCallButtonOutlet: UIButton!
     
     @IBOutlet weak var instructionsLabel: UILabel!
-    @IBOutlet weak var emergencyCallButtonOutlet: UIButton!
+    
     @IBAction func emergencyCallButtonAction(_ sender: Any) {
         if let phoneCallUrl = URL(string : "tel://514-396-8900") {
             let application:UIApplication = UIApplication.shared
@@ -20,25 +22,24 @@ class BombThreatViewController: UIViewController {
             }
         }
     }
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        instructionsLabel!.attributedText = NSMutableAttributedString(string: NSLocalizedString("bombThreatInstructions", comment: "Instructions for bomb threat"))
+
+        instructionsLabel!.attributedText = NSMutableAttributedString(string: NSLocalizedString("suspiciousPackageInstructions", comment: "Instructions for suspicious package"))
         
         emergencyCallButtonOutlet.layer.cornerRadius = 5
-    emergencyCallButtonOutlet.setTitle(NSLocalizedString("emergencyCall", comment: "Emergency call"), for: UIControlState.normal)
+        emergencyCallButtonOutlet.setTitle(NSLocalizedString("emergencyCall", comment: "Emergency call"), for: UIControlState.normal)
         // Do any additional setup after loading the view.
     }
 
-    
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    
+
     /*
     // MARK: - Navigation
 
@@ -49,6 +50,4 @@ class BombThreatViewController: UIViewController {
     }
     */
 
-    
-    
 }
