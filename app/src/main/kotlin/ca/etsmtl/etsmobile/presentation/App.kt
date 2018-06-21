dc77eb5ef774ebc5bc6598629a072a6ec1dfe5ef
Thanh-Son-Philippe Lam <thanh-son-philippe.lam.1@ens.etsmtl.ca>
2018-06-21 15:22:27 -0400

    Add Firebase and Crashlytics

--- app/src/main/kotlin/ca/etsmtl/etsmobile/presentation/App.kt
+++ app/src/main/kotlin/ca/etsmtl/etsmobile/presentation/App.kt
@@ -2,9 +2,13 @@ package ca.etsmtl.etsmobile.presentation
 
 import ca.etsmtl.etsmobile.di.DaggerAppComponent
 import ca.etsmtl.repos.di.RepositoryModule
+import com.crashlytics.android.BuildConfig
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.core.CrashlyticsCore
 import com.squareup.leakcanary.LeakCanary
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
+import io.fabric.sdk.android.Fabric
 
 /**
  * Created by Sonphil on 28-02-18.
@@ -20,6 +24,13 @@ class App : DaggerApplication() {
             return
         }
         LeakCanary.install(this)
+
+        // Initializes Fabric for builds that don't use the debug build type.
+        val crashlyticsKit = Crashlytics.Builder()
+                .core(CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
+                .build()
+
+        Fabric.with(this, crashlyticsKit)
     }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
