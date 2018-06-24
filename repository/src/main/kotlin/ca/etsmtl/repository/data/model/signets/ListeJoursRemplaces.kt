63c7ef1ed8a4e8b0d75adb98d1c8fce3ba880127
Thanh-Son-Philippe Lam <thanh-son-philippe.lam.1@ens.etsmtl.ca>
2018-06-24 13:06:27 -0400

    Add JourRemplaceRepository

--- repository/src/main/kotlin/ca/etsmtl/repository/data/model/signets/ListeJoursRemplaces.kt
+++ repository/src/main/kotlin/ca/etsmtl/repository/data/model/signets/ListeJoursRemplaces.kt
@@ -6,7 +6,7 @@ import se.ansman.kotshi.JsonSerializable
 @JsonSerializable
 data class ListeJoursRemplaces(
     @Json(name = "__type") val type: String? = "",
-    @Json(name = "listeJours") val listeJours: List<JourRemplace?>? = listOf(),
+    @Json(name = "listeJours") val listeJours: List<JourRemplace>? = listOf(),
     @Json(name = "erreur") val erreur: String? = ""
 ) : SignetsData() {
     override fun getError() = erreur
