c9b52913c4a31946864617a36e067187b3293696
Thanh-Son-Philippe Lam <thanh-son-philippe.lam.1@ens.etsmtl.ca>
2018-07-17 01:04:12 -0400

    Add JourRemplaceRepository and SeanceRepository

--- repository/src/main/kotlin/ca/etsmtl/repository/data/api/response/mapper/SignetsMapperExt.kt
+++ repository/src/main/kotlin/ca/etsmtl/repository/data/api/response/mapper/SignetsMapperExt.kt
@@ -5,11 +5,19 @@ import ca.etsmtl.repository.data.api.response.signets.ApiCours
 import ca.etsmtl.repository.data.api.response.signets.ApiEnseignant
 import ca.etsmtl.repository.data.api.response.signets.ApiEtudiant
 import ca.etsmtl.repository.data.api.response.signets.ApiEvaluation
+import ca.etsmtl.repository.data.api.response.signets.ApiHoraireExamenFinal
+import ca.etsmtl.repository.data.api.response.signets.ApiJourRemplace
+import ca.etsmtl.repository.data.api.response.signets.ApiListeDesSeances
+import ca.etsmtl.repository.data.api.response.signets.ApiListeJoursRemplaces
+import ca.etsmtl.repository.data.api.response.signets.ApiSeance
 import ca.etsmtl.repository.data.db.entity.signets.ActiviteEntity
 import ca.etsmtl.repository.data.db.entity.signets.CoursEntity
 import ca.etsmtl.repository.data.db.entity.signets.EnseignantEntity
 import ca.etsmtl.repository.data.db.entity.signets.EtudiantEntity
 import ca.etsmtl.repository.data.db.entity.signets.EvaluationEntity
+import ca.etsmtl.repository.data.db.entity.signets.HoraireExamenFinalEntity
+import ca.etsmtl.repository.data.db.entity.signets.JourRemplaceEntity
+import ca.etsmtl.repository.data.db.entity.signets.SeanceEntity
 
 /**
  * Created by Sonphil on 08-07-18.
@@ -73,3 +81,46 @@ fun ApiEvaluation.toEvaluationEntity() = EvaluationEntity(
         this.messageDuProf,
         this.ignoreDuCalcul
 )
+
+fun ApiHoraireExamenFinal.toHoraireExemanFinalEntity() = HoraireExamenFinalEntity(
+        this.sigle,
+        this.groupe,
+        this.dateExamen,
+        this.heureDebut,
+        this.heureFin,
+        this.local
+)
+
+fun ApiJourRemplace.toJourRemplaceEntity() = JourRemplaceEntity(
+        this.dateOrigine,
+        this.dateRemplacement,
+        this.description
+)
+
+fun ApiListeJoursRemplaces.toJourRemplaceEntities(): List<JourRemplaceEntity>? {
+    this.listeJours?.let {
+        return ArrayList<JourRemplaceEntity>().apply {
+            it.forEach {
+                this.add(it.toJourRemplaceEntity())
+            }
+        }
+    }
+
+    return null
+}
+
+fun ApiSeance.toSeanceEntity() = SeanceEntity(
+        this.dateDebut,
+        this.dateFin,
+        this.coursGroupe,
+        this.nomActivite,
+        this.local,
+        this.descriptionActivite,
+        this.libelleCours
+)
+
+fun ApiListeDesSeances.toSeancesEntities(): List<SeanceEntity> = ArrayList<SeanceEntity>().apply {
+    this@toSeancesEntities.liste.forEach {
+        add(it.toSeanceEntity())
+    }
+}
--- repository/src/main/kotlin/ca/etsmtl/repository/data/db/entity/mapper/SignetsMapperExt.kt
+++ repository/src/main/kotlin/ca/etsmtl/repository/data/db/entity/mapper/SignetsMapperExt.kt
@@ -1,7 +1,11 @@
 package ca.etsmtl.repository.data.db.entity.mapper
 
 import ca.etsmtl.repository.data.db.entity.signets.EtudiantEntity
+import ca.etsmtl.repository.data.db.entity.signets.JourRemplaceEntity
+import ca.etsmtl.repository.data.db.entity.signets.SeanceEntity
 import ca.etsmtl.repository.data.model.Etudiant
+import ca.etsmtl.repository.data.model.JourRemplace
+import ca.etsmtl.repository.data.model.Seance
 
 /**
  * Created by Sonphil on 09-07-18.
@@ -15,3 +19,31 @@ fun EtudiantEntity.toEtudiant() = Etudiant(
         this.soldeTotal,
         this.masculin
 )
+
+fun JourRemplaceEntity.toJourRemplace() = JourRemplace(
+        this.dateOrigine,
+        this.dateRemplacement,
+        this.description
+)
+
+fun List<JourRemplaceEntity>.toJoursRemplaces(): List<JourRemplace> = ArrayList<JourRemplace>().apply {
+    this@toJoursRemplaces.forEach {
+        add(it.toJourRemplace())
+    }
+}
+
+fun SeanceEntity.toSeance() = Seance(
+        this.dateDebut,
+        this.dateFin,
+        this.coursGroupe,
+        this.nomActivite,
+        this.local,
+        this.descriptionActivite,
+        this.libelleCours
+)
+
+fun List<SeanceEntity>.toSeances(): List<Seance> = ArrayList<Seance>().apply {
+    this@toSeances.forEach {
+        add(it.toSeance())
+    }
+}
