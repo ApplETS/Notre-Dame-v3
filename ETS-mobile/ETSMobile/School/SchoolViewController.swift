51960a521358a997f883a762619d5295729a3568
Antoine Lamy <antoinelamy@gmail.com>
2018-10-01 18:26:14 -0400

    Extract storyboards

---
+++ ETS-mobile/ETSMobile/School/SchoolViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SchoolViewController.swift
+//  ETSMobile
+//
+//  Created by Antoine Lamy on 2018-10-01.
+//  Copyright © 2018 ApplETS. All rights reserved.
+//
+
+import UIKit
+
+class SchoolViewController: UIViewController {
+    
+}
