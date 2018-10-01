43606b325d32363fea698bbcac61812b18b5f85d
Antoine Lamy <antoinelamy@gmail.com>
2018-10-01 18:47:24 -0400

    Move into groups

---
+++ ETSMobile/ETSMobile/Service/MonETSClient.swift
@@ -0,0 +1,13 @@
+//
+//  MonETSClient.swift
+//  ETSMobile
+//
+//  Created by Antoine Lamy on 2018-10-01.
+//  Copyright © 2018 ApplETS. All rights reserved.
+//
+
+import Foundation
+
+class MonETSClient {
+    
+}
