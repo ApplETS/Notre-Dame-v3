7a1aa19d45e53906e68a2d7d41d618de2aa7b5e9
Thanh-Son-Philippe Lam <thanh-son-philippe.lam.1@ens.etsmtl.ca>
2018-10-03 21:32:09 -0400

    Fix evaluation's arrow orientation
    When the view was bounded again, the orientation of the arrow was wrong if the group was expanded.

--- app/src/main/kotlin/ca/etsmtl/applets/etsmobile/util/ViewExt.kt
+++ app/src/main/kotlin/ca/etsmtl/applets/etsmobile/util/ViewExt.kt
@@ -4,6 +4,8 @@ import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.content.Context
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
 import android.view.inputmethod.InputMethodManager
 
 /**
@@ -52,5 +54,45 @@ fun View.hideKeyboard(): Boolean {
         inputMethodManager.hideSoftInputFromWindow(windowToken, 0)
     } catch (ignored: RuntimeException) {
         false
+    }
+}
+
+/**
+ * Runs an animation that rotates the view
+ *
+ * @param fromDegrees Rotation offset to apply at the start of the animation.
+ * @param toDegrees Rotation offset to apply at the end of the animation.
+ * @param pivotXType Specifies how pivotXValue should be interpreted. One of Animation.ABSOLUTE,
+ * Animation.RELATIVE_TO_SELF, or Animation.RELATIVE_TO_PARENT.
+ * @param pivotXValue The X coordinate of the point about which the object is being rotated,
+ * specified as an absolute number where 0 is the left edge. This value can either be an absolute
+ * number if pivotXType is ABSOLUTE, or a percentage (where 1.0 is 100%) otherwise.
+ * @param pivotYType Specifies how pivotYValue should be interpreted. One of Animation.ABSOLUTE,
+ * Animation.RELATIVE_TO_SELF, or Animation.RELATIVE_TO_PARENT.
+ * @param pivotYValue The Y coordinate of the point about which the object is being rotated,
+ * specified as an absolute number where 0 is the top edge. This value can either be an absolute
+ * number if pivotYType is ABSOLUTE, or a percentage (where 1.0 is 100%) otherwise.
+ * @param duration Amount of time (in milliseconds) for the animation to run.
+ */
+fun View.rotate(
+    fromDegrees: Float,
+    toDegrees: Float,
+    pivotXType: Int = Animation.RELATIVE_TO_SELF,
+    pivotXValue: Float = 0.5f,
+    pivotYType: Int = Animation.RELATIVE_TO_SELF,
+    pivotYValue: Float = 0.5f,
+    duration: Long = resources.getInteger(android.R.integer.config_shortAnimTime).toLong()
+) {
+    RotateAnimation(
+            fromDegrees,
+            toDegrees,
+            pivotXType,
+            pivotXValue,
+            pivotYType,
+            pivotYValue
+    ).apply {
+        this.duration = duration
+        fillAfter = true
+        startAnimation(this)
     }
 }
