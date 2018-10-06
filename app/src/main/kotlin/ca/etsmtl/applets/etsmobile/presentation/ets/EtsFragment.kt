17591ec9b96163d7ce05d677299e8f13ab8f1235
Thanh-Son-Philippe Lam <thanh-son-philippe.lam.1@ens.etsmtl.ca>
2018-10-05 20:09:55 -0400

    Add CollapsingToolbarLayout to EtsFragment
    It will display ETS' banners later on.

--- app/src/main/kotlin/ca/etsmtl/applets/etsmobile/presentation/ets/EtsFragment.kt
+++ app/src/main/kotlin/ca/etsmtl/applets/etsmobile/presentation/ets/EtsFragment.kt
@@ -1,12 +1,16 @@
 package ca.etsmtl.applets.etsmobile.presentation.ets
 
 import android.os.Bundle
+import android.support.design.widget.AppBarLayout
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import ca.etsmtl.applets.etsmobile.R
-import kotlinx.android.synthetic.main.include_toolbar.*
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.fragment_ets.appBarLayoutEts
+import kotlinx.android.synthetic.main.fragment_ets.iVETSLogo
+import kotlinx.android.synthetic.main.fragment_ets.ivEtsAppBar
 
 /**
  * This fragment shows information related to the university.
@@ -15,11 +19,6 @@ import kotlinx.android.synthetic.main.include_toolbar.*
  */
 
 class EtsFragment : Fragment() {
-
-    companion object {
-        private const val ETS_FRAGMENT_TAG = "EtsFragment"
-        fun newInstance() = EtsFragment()
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,6 +32,30 @@ class EtsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        toolbar.setTitle(R.string.title_ets)
+        setUpToolBar()
+    }
+
+    private fun setUpToolBar() {
+        appBarLayoutEts.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
+            val maxScroll = appBarLayout.totalScrollRange
+            val ratio = Math.abs(verticalOffset) / maxScroll.toFloat()
+
+            iVETSLogo.alpha = when {
+                ratio >= MIN_VERTICAL_OFFSET_BEFORE_SHOWING_LOGO -> {
+                    (ratio - MIN_VERTICAL_OFFSET_BEFORE_SHOWING_LOGO) / (1 - MIN_VERTICAL_OFFSET_BEFORE_SHOWING_LOGO)
+                }
+                else -> 0f
+            }
+        })
+
+        Glide.with(this)
+                .load("https://etsmtl.ca/ETS/media/Prive/Accueil/slideshow/entete1-1.jpg")
+                .into(ivEtsAppBar)
+    }
+
+    companion object {
+        private const val TAG = "EtsFragment"
+        private const val MIN_VERTICAL_OFFSET_BEFORE_SHOWING_LOGO = 0.3f
+        fun newInstance() = EtsFragment()
     }
 }
