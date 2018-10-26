f31c9bdc27d257847339179a8909918c1c98f162
Emmanuel Proulx <foufoulefou2juillet@hotmail.com>
2018-10-25 20:32:10 -0400

    Add password hide option

--- ETSMobile/ETSMobile/UI/Common/ETSTextField.swift
+++ ETSMobile/ETSMobile/UI/Common/ETSTextField.swift
@@ -37,4 +37,26 @@ class ETSTextField: UITextField {
         }
         self.tintColor = UIColor.white;
     }
+    
+    public func togglePasswordVisibility() {
+        isSecureTextEntry = !isSecureTextEntry
+        
+        if let existingText = text, isSecureTextEntry {
+            /* When toggling to secure text, all text will be purged if the user
+             continues typing unless we intervene. This is prevented by first
+             deleting the existing text and then recovering the original text. */
+            deleteBackward()
+            
+            if let textRange = textRange(from: beginningOfDocument, to: endOfDocument) {
+                replace(textRange, withText: existingText)
+            }
+        }
+        
+        /* Reset the selected text range since the cursor can end up in the wrong
+         position after a toggle because the text might vary in width */
+        if let existingSelectedTextRange = selectedTextRange {
+            selectedTextRange = nil
+            selectedTextRange = existingSelectedTextRange
+        }
+    }
 }
--- ETSMobile/ETSMobile/UI/Login/LoginViewController.swift
+++ ETSMobile/ETSMobile/UI/Login/LoginViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    var isSecureTextEntry = true
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
@@ -23,6 +25,9 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var password: ETSTextField!
     @IBOutlet weak var loginButtonContent: ETSButton!
     
+    let passwordRightSideButton = UIButton(type: .custom)
+    var passwordHidden : Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(
@@ -32,18 +37,30 @@ class LoginViewController: UIViewController {
         self.view.addGestureRecognizer(tap)
         username!.placeholder = NSLocalizedString("username", comment: "Access code")
         password!.placeholder = NSLocalizedString("password", comment: "Password")
-        let rightSideButton = UIButton(type: .custom)
         
-        rightSideButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: -16, bottom: 0, right: 0)
-        rightSideButton.frame = CGRect(x: CGFloat(username!.frame.size.width - 25), y: CGFloat(5), width: CGFloat(25), height: CGFloat(25))
+        let usernameRightSideButton = UIButton(type: .custom)
+        usernameRightSideButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: -16, bottom: 0, right: 0)
+        usernameRightSideButton.frame = CGRect(x: CGFloat(username!.frame.size.width - 25), y: CGFloat(5), width: CGFloat(25), height: CGFloat(25))
         let disclosure = UITableViewCell()
-        disclosure.frame = rightSideButton.bounds
+        disclosure.frame = usernameRightSideButton.bounds
         disclosure.accessoryType = .detailButton
         disclosure.isUserInteractionEnabled = false
-        rightSideButton.addSubview(disclosure)
-        rightSideButton.addTarget(self, action: #selector(UsernameInfo), for: .touchUpInside)
-        username!.rightView = rightSideButton
+        usernameRightSideButton.addSubview(disclosure)
+        usernameRightSideButton.addTarget(self, action: #selector(usernameInfo), for: .touchUpInside)
+        username!.rightView = usernameRightSideButton
         username!.rightViewMode = .always
+        
+        passwordRightSideButton.setImage(UIImage(named: "eyeClosed"), for: .normal)
+        passwordRightSideButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: -16, bottom: 0, right: 0)
+        passwordRightSideButton.frame = CGRect(x: CGFloat(username!.frame.size.width - 25), y: CGFloat(5), width: CGFloat(25), height: CGFloat(25))
+        
+        passwordRightSideButton.addTarget(self, action: #selector(passwordToggle), for: .touchUpInside)
+        password!.rightView = passwordRightSideButton
+        password!.rightViewMode = .always
+        
+        
+        
+        
         
         
         madeBy!.text = NSLocalizedString("madeBy", comment: "Réalisé par")
@@ -59,13 +76,25 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @objc func UsernameInfo(sender: UIButton!) {
+    @objc func usernameInfo(sender: UIButton!) {
         let alert = UIAlertController(title: NSLocalizedString("universalAccessCode", comment: "universalAccessCode"), message: NSLocalizedString("universalAccessCodeInfo", comment: "universalAccessCodeInfo"), preferredStyle: .actionSheet)
         
         alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
         
         self.present(alert, animated: true)
     }
+    
+    @objc func passwordToggle(sender: UIButton!) {
+        password!.togglePasswordVisibility()
+        passwordHidden = !passwordHidden
+        if passwordHidden {
+            passwordRightSideButton.setImage(UIImage(named: "eyeClosed"), for: .normal)
+        } else {
+            passwordRightSideButton.setImage(UIImage(named: "eyeOpen"), for: .normal)
+        }
+        
+    }
+    
 
     /*
     // MARK: - Navigation
@@ -78,3 +107,5 @@ class LoginViewController: UIViewController {
     */
 
 }
+
+
