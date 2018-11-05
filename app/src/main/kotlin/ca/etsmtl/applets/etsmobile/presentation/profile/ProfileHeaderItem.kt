2c1b1e56858aee5a2f8ea100a438fb8bb57ffd54
Thanh-Son-Philippe Lam <thanh-son-philippe.lam.1@ens.etsmtl.ca>
2018-11-05 00:12:59 -0500

    Change ProfileHeaderItem and ProfileValueItem to data classes

--- app/src/main/kotlin/ca/etsmtl/applets/etsmobile/presentation/profile/ProfileHeaderItem.kt
+++ app/src/main/kotlin/ca/etsmtl/applets/etsmobile/presentation/profile/ProfileHeaderItem.kt
@@ -9,27 +9,11 @@ import kotlinx.android.synthetic.main.item_profile_header.tvTitleProfile
  * Created by Sonphil on 27-10-18.
  */
 
-class ProfileHeaderItem(val title: String) : ProfileItem<ProfileAdapter.ProfileViewHolder.HeaderViewHolder> {
+data class ProfileHeaderItem(val title: String) : ProfileItem<ProfileAdapter.ProfileViewHolder.HeaderViewHolder> {
     override fun getLayout() = R.layout.item_profile_header
 
     override fun bind(viewHolder: ProfileAdapter.ProfileViewHolder.HeaderViewHolder, position: Int) {
         viewHolder.tvTitleProfile.text = title
         viewHolder.dividerProfile.show(position != 0)
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) {
-            return true
-        }
-
-        if (other == null || javaClass != other.javaClass) {
-            return false
-        }
-
-        return title == (other as ProfileHeaderItem).title
-    }
-
-    override fun hashCode(): Int {
-        return title.hashCode()
     }
 }
--- app/src/main/kotlin/ca/etsmtl/applets/etsmobile/presentation/profile/ProfileValueItem.kt
+++ app/src/main/kotlin/ca/etsmtl/applets/etsmobile/presentation/profile/ProfileValueItem.kt
@@ -8,7 +8,7 @@ import kotlinx.android.synthetic.main.item_profile.tvProfileItemValue
  * Created by Sonphil on 27-10-18.
  */
 
-class ProfileValueItem(
+data class ProfileValueItem(
     val label: String,
     val value: String
 ) : ProfileItem<ProfileAdapter.ProfileViewHolder.ItemViewHolder> {
@@ -19,25 +19,5 @@ class ProfileValueItem(
             tvInfoProfileItemLabel.text = label
             tvProfileItemValue.text = value
         }
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) {
-            return true
-        }
-
-        if (other == null || javaClass != other.javaClass) {
-            return false
-        }
-
-        with (other as ProfileValueItem) {
-            return label == other.label && value == other.value
-        }
-    }
-
-    override fun hashCode(): Int {
-        var result = label.hashCode()
-        result = 31 * result + value.hashCode()
-        return result
     }
 }
