03ee50b9caa33247c60c7c1a121bd9ef83ccd915
mykaelll87 <mykaelll87@gmail.com>
2018-12-09 22:20:58 -0500

    Importé le stuff de l'autre repo par rapport à l'Horaire

--- android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/presentation/schedule/ScheduleFragment.kt
+++ android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/presentation/schedule/ScheduleFragment.kt
@@ -14,29 +14,29 @@ import ca.etsmtl.applets.etsmobile.R
 import ca.etsmtl.applets.etsmobile.util.EventObserver
 import ca.etsmtl.applets.etsmobile.util.isVisible
 import dagger.android.support.DaggerFragment
-import kotlinx.android.synthetic.main.empty_view_schedule.emptyViewSchedule
-import kotlinx.android.synthetic.main.fragment_schedule.recyclerViewSchedule
-import kotlinx.android.synthetic.main.fragment_schedule.swipeRefreshLayoutSchedule
+import jp.wasabeef.recyclerview.animators.FadeInLeftAnimator
+import kotlinx.android.synthetic.main.empty_view_schedule.*
+import kotlinx.android.synthetic.main.fragment_schedule.*
+import java.util.Calendar
+import java.util.Date
 import javax.inject.Inject
 
 /**
  * Created by Sonphil on 25-02-18.
  */
-
 class ScheduleFragment : DaggerFragment() {
     private val scheduleViewModel: ScheduleViewModel by lazy {
         ViewModelProviders.of(this, viewModelFactory).get(ScheduleViewModel::class.java)
     }
+
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
-
     private val adapter: ScheduleAdapter = ScheduleAdapter()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_schedule, container, false)
     }
 
@@ -56,11 +56,40 @@ class ScheduleFragment : DaggerFragment() {
     private fun setUpRecyclerView() {
         recyclerViewSchedule.adapter = adapter
         recyclerViewSchedule.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        recyclerViewSchedule.itemAnimator = FadeInLeftAnimator()
     }
+
     private fun subscribeUI() {
-        scheduleViewModel.seances.observe(this, Observer {
-            it?.takeIf { it.isNotEmpty() }?.let { adapter.items = it }
+        scheduleViewModel.sessions.observe(this, Observer {
+            // TODO utiliser les sessions dans un option menu
         })
+        scheduleViewModel.seances.observe(this, Observer {
+            it?.takeIf { it.isNotEmpty() }?.let { seances ->
+                val thisWeekCal = Calendar.getInstance()
+                val wantedWeek = thisWeekCal.get(Calendar.WEEK_OF_YEAR)
+                val wantedYear = thisWeekCal.get(Calendar.YEAR)
+                adapter.items = seances
+                    .filter { s ->
+                        val dateCal = Calendar.getInstance()
+                        dateCal.time = s.dateDebut
+                        val dateWeek = dateCal.get(Calendar.WEEK_OF_YEAR)
+                        val dateYear = dateCal.get(Calendar.YEAR)
+
+                        wantedWeek == dateWeek && wantedYear == dateYear
+                    } // todo filter for the selected week
+                    .groupBy { s ->
+                        val cal = Calendar.getInstance()
+                        cal.clear()
+                        cal.time = s.dateDebut
+                        cal.set(Calendar.HOUR, 0)
+                        cal.set(Calendar.MINUTE, 0)
+                        cal.set(Calendar.SECOND, 0)
+                        cal.set(Calendar.AM_PM, 0)
+                        Date(cal.timeInMillis)
+                    }
+            }
+        })
+
         scheduleViewModel.showEmptyView.observe(this, Observer {
             recyclerViewSchedule.isVisible = !it
             emptyViewSchedule.isVisible = it
@@ -71,6 +100,10 @@ class ScheduleFragment : DaggerFragment() {
 
         scheduleViewModel.errorMessage.observe(this, EventObserver {
             it?.let { Toast.makeText(context, it, Toast.LENGTH_LONG).show() }
+        })
+
+        scheduleViewModel.selectedSession.observe(this, Observer {
+            scheduleViewModel.loadSeances()
         })
 
         this.lifecycle.addObserver(scheduleViewModel)
--- android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/presentation/schedule/ScheduleViewModel.kt
+++ android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/presentation/schedule/ScheduleViewModel.kt
@@ -7,40 +7,59 @@ import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.OnLifecycleEvent
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
-import ca.etsmtl.applets.etsmobile.R
-import ca.etsmtl.applets.etsmobile.domain.FetchCurrentSessionSeancesUseCase
+import ca.etsmtl.applets.etsmobile.domain.FetchSessionSeancesUseCase
+import ca.etsmtl.applets.etsmobile.domain.FetchSessionsUseCase
 import ca.etsmtl.applets.etsmobile.presentation.App
 import ca.etsmtl.applets.etsmobile.util.Event
-import ca.etsmtl.applets.etsmobile.util.isDeviceConnected
+import ca.etsmtl.applets.etsmobile.util.getGenericErrorMessage
 import ca.etsmtl.applets.repository.data.model.Resource
 import ca.etsmtl.applets.repository.data.model.Seance
-import ca.etsmtl.applets.repository.data.model.SignetsUserCredentials
+import ca.etsmtl.applets.repository.data.model.Session
 import javax.inject.Inject
 
 /**
  * Created by mykaelll87 on 2018-10-24
  */
 class ScheduleViewModel @Inject constructor(
-    private var userCredentials: SignetsUserCredentials,
-    private val fetchCurrentSessionSeancesUseCase: FetchCurrentSessionSeancesUseCase,
+    private val fetchSessionSeancesUseCase: FetchSessionSeancesUseCase,
+    private val fetchSessionsUseCase: FetchSessionsUseCase,
     private val app: App
 ) : ViewModel(), LifecycleObserver {
+
+    // Seances
     private val seancesMediatorLiveData: MediatorLiveData<Resource<List<Seance>>> by lazy {
         MediatorLiveData<Resource<List<Seance>>>()
     }
     private var seancesLiveData: LiveData<Resource<List<Seance>>>? = null
+    // Sessions
+    private val sessionsMediatorLiveData: MediatorLiveData<Resource<List<Session>>> by lazy {
+        MediatorLiveData<Resource<List<Session>>>()
+    }
+    private var sessionsLiveData: LiveData<Resource<List<Session>>>? = null
+
+    // TODO binder ça au option menu des sessions
+    val selectedSessionMediatorLiveData: MediatorLiveData<Session> by lazy {
+        MediatorLiveData<Session>()
+    }
+
+    val selectedSession: LiveData<Session> = Transformations.map(selectedSessionMediatorLiveData) {
+        it
+    }
 
     val errorMessage: LiveData<Event<String?>> by lazy {
-        Transformations.map(seancesMediatorLiveData) {
+        val sessionMessage = Transformations.map(sessionsMediatorLiveData) {
             if (it.status == Resource.Status.ERROR) {
-                when {
-                    !app.isDeviceConnected() -> {
-                        Event(app.getString(R.string.error_no_internet_connection))
-                    }
-                    else -> Event(app.getString(R.string.error))
-                }
+                it.getGenericErrorMessage(app)
             } else {
-                Event(it.message)
+                Event(null)
+            }
+        }
+
+        if (sessionMessage.value != null) {
+            return@lazy sessionMessage
+        } else {
+            return@lazy Transformations.map(seancesMediatorLiveData) {
+                it.getGenericErrorMessage(app)
             }
         }
     }
@@ -48,26 +67,61 @@ class ScheduleViewModel @Inject constructor(
         it.data
     }
 
+    val sessions: LiveData<List<Session>> = Transformations.map(sessionsMediatorLiveData) {
+        it.data
+    }
+
     val loading: LiveData<Boolean> = Transformations.map(seancesMediatorLiveData) {
         it.status == Resource.Status.LOADING
     }
-
+    // Todo Peut-être pas tant utile, le clearer si ça l'est pas
+    val showNoSessionsView: LiveData<Boolean> = Transformations.map(sessionsMediatorLiveData) {
+        it.status != Resource.Status.LOADING && (it?.data == null || it.data?.isEmpty() == true)
+    }
     val showEmptyView: LiveData<Boolean> = Transformations.map(seancesMediatorLiveData) {
         it.status != Resource.Status.LOADING && (it?.data == null || it.data?.isEmpty() == true)
     }
 
-    private fun load() {
-        seancesLiveData = fetchCurrentSessionSeancesUseCase(userCredentials).apply {
-            seancesMediatorLiveData.addSource(this) {
-                seancesMediatorLiveData.value = it
+    fun loadSeances() {
+        seancesLiveData?.let { seancesMediatorLiveData.removeSource(it) }
+        selectedSession.value?.let { s ->
+            seancesLiveData = fetchSessionSeancesUseCase(s).apply {
+                seancesMediatorLiveData.addSource(this) {
+                    seancesMediatorLiveData.value = it
+                }
+            }
+        }
+    }
+
+    private fun loadSessions() {
+        sessionsLiveData = fetchSessionsUseCase().apply {
+            sessionsMediatorLiveData.addSource(this) {
+                sessionsMediatorLiveData.value = it
+            }
+            selectedSessionMediatorLiveData.addSource(this) {
+                var currentSession = it.data.orEmpty()
+                    .firstOrNull { s -> System.currentTimeMillis() in s.dateDebut..s.dateFin }
+                if (currentSession == null) {
+                    currentSession = if (it.data?.isNotEmpty() == true) it.data?.get(0) else null
+                }
+
+                selectedSessionMediatorLiveData.value = currentSession
             }
         }
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
     fun refresh() {
+        // Sessions
+        sessionsLiveData?.let {
+            sessionsMediatorLiveData.removeSource(it)
+            selectedSessionMediatorLiveData.removeSource(it)
+        }
+        sessionsLiveData = null
+
+        // Seances
         seancesLiveData?.let { seancesMediatorLiveData.removeSource(it) }
         seancesLiveData = null
-        load()
+        loadSessions()
     }
 }
