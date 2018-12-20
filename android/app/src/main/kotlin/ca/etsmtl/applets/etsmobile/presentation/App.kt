2126ce4e7c4579f110c6b3d99f5108abe04ed493
Thanh-Son-Philippe Lam <thanh-son-philippe.lam.1@ens.etsmtl.ca>
2018-12-19 22:01:59 -0500

    Get Buglife api key from res

--- android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/presentation/App.kt
+++ android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/presentation/App.kt
@@ -30,7 +30,7 @@ class App : DaggerApplication() {
     }
 
     private fun setupBuglife() {
-        Buglife.initWithApiKey(this, "TODO") // TODO: Insert Buglife key here
+        Buglife.initWithApiKey(this, getString(R.string.buglife_key))
         Buglife.setInvocationMethod(InvocationMethod.BUG_BUTTON)
         Buglife.setInvocationMethod(InvocationMethod.SHAKE)
 
