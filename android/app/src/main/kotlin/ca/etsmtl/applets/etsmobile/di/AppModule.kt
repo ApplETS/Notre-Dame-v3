003f7842ad1b163df59ae7162108ce72493a4015
Thanh-Son-Philippe Lam <thanh-son-philippe.lam.1@ens.etsmtl.ca>
2018-12-29 22:57:47 -0500

    Move prefs key and signets url to repository module and let the module provide the prefs

--- android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/di/AppModule.kt
+++ android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/di/AppModule.kt
@@ -2,8 +2,6 @@ package ca.etsmtl.applets.etsmobile.di
 
 import android.app.Application
 import android.content.Context
-import android.content.SharedPreferences
-import ca.etsmtl.applets.etsmobile.R
 import ca.etsmtl.applets.etsmobile.presentation.App
 import ca.etsmtl.applets.repository.data.model.SignetsUserCredentials
 import dagger.Module
@@ -24,13 +22,6 @@ internal object AppModule {
     @Provides
     @JvmStatic
     fun provideApp(application: Application): App = application as App
-
-    @Singleton
-    @Provides
-    @JvmStatic
-    fun providePrefs(application: Application): SharedPreferences = application
-            .getSharedPreferences(application.getString(R.string.preference_file_key),
-                    Context.MODE_PRIVATE)
 
     @Singleton
     @Provides
--- android/repository/src/main/kotlin/ca/etsmtl/applets/repository/di/RepositoryModule.kt
+++ android/repository/src/main/kotlin/ca/etsmtl/applets/repository/di/RepositoryModule.kt
@@ -1,6 +1,12 @@
 package ca.etsmtl.applets.repository.di
 
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
+import ca.etsmtl.applets.repository.R
 import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
 
 /**
  * Created by Sonphil on 07-06-18.
@@ -10,8 +16,10 @@ import dagger.Module
     DatabaseModule::class,
     NetworkModule::class
 ])
-class RepositoryModule {
-    companion object {
-        val instance = RepositoryModule()
-    }
+class RepositoryModule(private val application: Application) {
+    @Singleton
+    @Provides
+    fun providePrefs(): SharedPreferences = application
+        .getSharedPreferences(application.getString(R.string.preference_file_key),
+            Context.MODE_PRIVATE)
 }
