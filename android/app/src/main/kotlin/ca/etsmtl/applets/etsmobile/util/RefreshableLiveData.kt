a471c5fdd53bb13859c186fb228d245b199f4d44
Thanh-Son-Philippe Lam <thanh-son-philippe.lam.1@ens.etsmtl.ca>
2019-01-11 16:56:02 -0500

    Add livedata-ktx and RefreshableLiveData (#21)
    * Add livedata-ktx and RefreshableLiveData and use them, for now, in GradesDetailsViewModel
    
    * Move zeroIfNullOrBlank calls to mapper

---
+++ android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/util/RefreshableLiveData.kt
@@ -0,0 +1,28 @@
+package ca.etsmtl.applets.etsmobile.util
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+
+/**
+ * Created by Sonphil on 10-01-19.
+ */
+
+abstract class RefreshableLiveData<T> : MediatorLiveData<T>() {
+    private var source: LiveData<T>? = null
+        set(value) {
+            field?.let { removeSource(it) }
+            field = value
+        }
+
+    fun refresh() {
+        updateSource().let {
+            source = it
+
+            addSource(it) { value ->
+                this.value = value
+            }
+        }
+    }
+
+    abstract fun updateSource(): LiveData<T>
+}
