3910c00c361ab42b58ed4ebbf0c2fa2196279ec4
Thanh-Son-Philippe Lam <thanh-son-philippe.lam.1@ens.etsmtl.ca>
2019-01-19 13:03:57 -0500

    Add initial setup
    Based on the tutorial available on https://kotlinlang.org/docs/tutorials/native/mpp-ios-android.html

---
+++ shared/src/androidMain/kotlin/actual.kt
@@ -0,0 +1,3 @@
+package org.kotlin.mpp.mobile
+
+actual fun platformName() = "Android"
---
+++ shared/src/commonMain/kotlin/common.kt
@@ -0,0 +1,7 @@
+package org.kotlin.mpp.mobile
+
+expect fun platformName(): String
+
+fun createApplicationScreenMessage() : String {
+    return "Kotlin Rocks on ${platformName()}"
+}
