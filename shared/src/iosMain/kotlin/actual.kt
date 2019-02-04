09eccd13762b9dc07d42bdd616a16c8bfb1f29c5
Thanh-Son-Philippe Lam <thanh-son-philippe.lam.1@ens.etsmtl.ca>
2019-02-04 18:30:55 -0500

    Setup Kotlin/Native (#37)
    * Add initial setup
    
    Based on the tutorial available on https://kotlinlang.org/docs/tutorials/native/mpp-ios-android.html
    
    * Add packForXCode task

---
+++ shared/src/iosMain/kotlin/actual.kt
@@ -0,0 +1,8 @@
+package org.kotlin.mpp.mobile
+
+import platform.UIKit.UIDevice
+
+actual fun platformName(): String {
+    return UIDevice.currentDevice.systemName() +
+            " " +
+            UIDevice.currentDevice.systemVersion
