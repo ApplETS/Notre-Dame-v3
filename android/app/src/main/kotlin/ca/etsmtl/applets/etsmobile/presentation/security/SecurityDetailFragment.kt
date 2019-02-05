f72b6b971f54f935ebaf2a2b00cc48e2c0d6c6af
MachoBandido08 <ricardo.gomez-cuadrado.1@etsmtl.net>
2019-02-05 16:41:30 -0500

    Create xml values files for different build/flavor
    * Remove gradle code

--- android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/presentation/security/SecurityDetailFragment.kt
+++ android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/presentation/security/SecurityDetailFragment.kt
@@ -3,6 +3,7 @@ package ca.etsmtl.applets.etsmobile.presentation.security
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
