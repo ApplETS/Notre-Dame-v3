5b467f1f73fe6f43a7a57d9e5e47b706457ec65a
Mykael Lemieux-Lafontaine <mykaelll87@gmail.com>
2019-02-15 22:19:28 -0500

    Schedule Dashboard Card

--- android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/presentation/dashboard/card/todayschedule/TodayScheduleCardFragment.kt
+++ android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/presentation/dashboard/card/todayschedule/TodayScheduleCardFragment.kt
@@ -4,19 +4,69 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import ca.etsmtl.applets.etsmobile.R
+import ca.etsmtl.applets.etsmobile.presentation.schedule.ScheduleInnerListAdapter
+import ca.etsmtl.applets.etsmobile.util.EventObserver
+import com.shopify.livedataktx.observe
+import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.fragment_today_schedule_card.*
+import javax.inject.Inject
 
 /**
  * Created by Sonphil on 24-01-19.
  */
 
-class TodayScheduleCardFragment : Fragment() {
+class TodayScheduleCardFragment : DaggerFragment() {
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    private val adapter: ScheduleInnerListAdapter = ScheduleInnerListAdapter()
+    private val todayScheduleCardViewModel: TodayScheduleCardViewModel by lazy {
+        ViewModelProviders.of(this, viewModelFactory).get(TodayScheduleCardViewModel::class.java)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View = inflater.inflate(R.layout.fragment_today_schedule_card, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUpRecyclerView()
+        subscribeUI()
+    }
+
+    private fun setUpRecyclerView() {
+        todayRecyclerview.adapter = adapter
+        todayRecyclerview.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+    }
+
+    private fun subscribeUI() {
+        todayScheduleCardViewModel.seances.observe(this) {
+            it?.takeIf { it.isNotEmpty() }?.let { seances ->
+                adapter.items = seances
+            }
+        }
+
+        todayScheduleCardViewModel.showEmptyView.observe(this) {
+            it?.let {
+                todayRecyclerview.isVisible = !it
+                emptyViewToday.isVisible = it
+            }
+        }
+
+        todayScheduleCardViewModel.errorMessage.observe(this, EventObserver {
+            it?.let { Toast.makeText(context, it, Toast.LENGTH_LONG).show() }
+        })
+    }
 
     companion object {
         val TAG = "TodayScheduleCardFragment"
---
+++ android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/presentation/dashboard/card/todayschedule/TodayScheduleCardViewModel.kt
@@ -0,0 +1,42 @@
+package ca.etsmtl.applets.etsmobile.presentation.dashboard.card.todayschedule
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import ca.etsmtl.applets.etsmobile.R
+import ca.etsmtl.applets.etsmobile.domain.FetchTodaySeancesUseCase
+import ca.etsmtl.applets.etsmobile.presentation.App
+import ca.etsmtl.applets.etsmobile.util.Event
+import ca.etsmtl.applets.etsmobile.util.getGenericErrorMessage
+import ca.etsmtl.applets.repository.data.model.Resource
+import com.shopify.livedataktx.map
+import com.shopify.livedataktx.nonNull
+import javax.inject.Inject
+
+/**
+Created by mykaelll87 on 15/02/19
+ */
+class TodayScheduleCardViewModel @Inject constructor(
+    private val fetchTodaySeancesUseCase: FetchTodaySeancesUseCase,
+    private val app: App
+) : ViewModel() {
+    private var seancesRes = fetchTodaySeancesUseCase()
+
+    val errorMessage: LiveData<Event<String?>> = seancesRes.nonNull().map {
+        if (app.getString(R.string.msg_api_no_seance) == it.message) {
+            Event(null)
+        } else
+            it.getGenericErrorMessage(app)
+    }
+
+    val seances = seancesRes.nonNull().map {
+        it.data
+    }
+//
+//    val loading: LiveData<Boolean> = seancesRes.map {
+//        it==null|| it.status == Resource.Status.LOADING
+//    }
+
+    val showEmptyView: LiveData<Boolean> = seancesRes.nonNull().map {
+        it.status != Resource.Status.LOADING && (it.data == null || it.data?.isEmpty() == true)
+    }
+}
