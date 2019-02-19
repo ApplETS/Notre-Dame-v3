8715643c4edba9ef3d503ed55245e50d5aa742ee
Thanh-Son-Philippe Lam <thanh-son-philippe.lam.1@ens.etsmtl.ca>
2019-02-19 02:00:07 -0500

    Make FetchTodaySeancesUseCase display data even if the status is "loading"

--- android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/domain/FetchTodaySeancesUseCase.kt
+++ android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/domain/FetchTodaySeancesUseCase.kt
@@ -1,11 +1,11 @@
 package ca.etsmtl.applets.etsmobile.domain
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
-import androidx.lifecycle.Transformations
 import ca.etsmtl.applets.etsmobile.R
 import ca.etsmtl.applets.etsmobile.presentation.App
 import ca.etsmtl.applets.repository.data.model.Resource
+import com.shopify.livedataktx.map
+import com.shopify.livedataktx.nonNull
 import model.Seance
 import java.util.Calendar
 import javax.inject.Inject
@@ -17,31 +17,32 @@ class FetchTodaySeancesUseCase @Inject constructor(
     private val fetchCurrentSessionSeancesUseCase: FetchCurrentSessionSeancesUseCase,
     private val app: App
 ) {
-    operator fun invoke(): LiveData<Resource<List<Seance>>> = Transformations.switchMap(fetchCurrentSessionSeancesUseCase()) { res ->
-        val mediatorLiveData = MediatorLiveData<Resource<List<Seance>>>()
-
-        when (res.status) {
-            Resource.Status.LOADING ->
-                mediatorLiveData.value = Resource.loading(emptyList())
-            Resource.Status.ERROR ->
-                mediatorLiveData.value = Resource.error(
-                    res.message ?: app.getString(R.string.error), emptyList()
-                )
-            Resource.Status.SUCCESS -> {
-                val start = Calendar.getInstance()
-                start.set(Calendar.HOUR_OF_DAY, 0)
-                start.set(Calendar.MINUTE, 0)
-                start.set(Calendar.SECOND, 0)
-                val end = start.clone() as Calendar
-                end.add(Calendar.DAY_OF_YEAR, 1)
-
-                val seances = res.data?.filter {
-                    it.dateDebut.unixMillis in start.timeInMillis..end.timeInMillis
+    operator fun invoke(): LiveData<Resource<List<Seance>>> = fetchCurrentSessionSeancesUseCase()
+        .nonNull()
+        .map { res ->
+            when (res.status) {
+                Resource.Status.LOADING ->
+                    Resource.loading(res.data.filterToday())
+                Resource.Status.ERROR ->
+                    Resource.error(
+                        res.message ?: app.getString(R.string.error), emptyList()
+                    )
+                Resource.Status.SUCCESS -> {
+                    Resource.success(res.data.filterToday())
                 }
-
-                mediatorLiveData.value = Resource.success(seances.orEmpty())
             }
         }
-        mediatorLiveData
-    }
+
+    private fun List<Seance>?.filterToday(): List<Seance> = this?.let {
+        val start = Calendar.getInstance()
+        start.set(Calendar.HOUR_OF_DAY, 0)
+        start.set(Calendar.MINUTE, 0)
+        start.set(Calendar.SECOND, 0)
+        val end = start.clone() as Calendar
+        end.add(Calendar.DAY_OF_YEAR, 1)
+
+        it.filter {
+            it.dateDebut.unixMillis.toLong() in start.timeInMillis..end.timeInMillis
+        }
+    }.orEmpty()
 }
