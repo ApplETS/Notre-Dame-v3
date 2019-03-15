62b7cfc5158d618be2c7485f05e7c6c635618d76
Thanh-Son-Philippe Lam <thanh-son-philippe.lam.1@ens.etsmtl.ca>
2019-03-15 00:58:33 -0400

    Handle evaluations not completed in grades details

---
+++ android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/domain/FetchCourseEvaluationsCompletedUseCase.kt
@@ -0,0 +1,39 @@
+package ca.etsmtl.applets.etsmobile.domain
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import ca.etsmtl.applets.repository.data.model.Resource
+import ca.etsmtl.applets.repository.data.repository.signets.EvaluationCoursRepository
+import model.Cours
+import model.EvaluationCours
+import model.SignetsUserCredentials
+import javax.inject.Inject
+
+/**
+ * Created by Sonphil on 15-03-19.
+ */
+
+class FetchCourseEvaluationsCompletedUseCase @Inject constructor(
+    private var userCredentials: SignetsUserCredentials,
+    private val evaluationCoursRepository: EvaluationCoursRepository
+) {
+    operator fun invoke(cours: Cours): LiveData<Resource<Boolean>> {
+        return Transformations.map(evaluationCoursRepository.getEvaluationCours(
+            userCredentials,
+            cours,
+            true
+        )) { evaluationCoursRes ->
+            val evaluationsCompleted = evaluationCoursRes.data?.areCourseEvaluationsCompletedForCourse(cours) ?: false
+
+            if (evaluationCoursRes.status == Resource.Status.SUCCESS) {
+                Resource.success(evaluationsCompleted)
+            } else {
+                evaluationCoursRes.copyStatusAndMessage(evaluationsCompleted)
+            }
+        }
+    }
+
+    private fun List<EvaluationCours>.areCourseEvaluationsCompletedForCourse(cours: Cours): Boolean {
+        return find { !it.estComplete && it.sigle == cours.sigle } == null
+    }
+}
