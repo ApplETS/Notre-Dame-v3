429ad90b47c8513098a20e6c4692101979c98cdf
zaclimon <isaacpateau05@gmail.com>
2019-04-20 15:10:42 -0400

    Adapt build for Ubuntu Xenial (16.04)
    - Change the OS to Xenial with minimal language in travis.yml
    - Download and extract the Android SDK manually
    - Change the PATH accordingly in travis.yml
    - Downgrade the version of JDK used to JDK8 (A more recent version is used)
    - Add android-wait-for-emulator script for waiting on a booted instance
    of the Android emulator for instrumentation tests
    
    Configuration snippets based from:
    https://travis-ci.com/G00fY2/android-ci-testproject/jobs/187085499/config

--- android/scripts/before_install.sh
+++ android/scripts/before_install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+ANDROID_API_LEVEL=28
+ANDROID_BUILD_TOOLS_VERSION=28.0.3
+# SDK Tools Revision 26.1.1
+ANDROID_SDK_TOOLS=sdk-tools-linux-4333796.zip
+# For Updates check https://developer.android.com/studio#downloads
+
 lsb_release -a
 wget https://www.openssl.org/source/openssl-1.1.0g.tar.gz > /dev/null
 tar xzvf openssl-1.1.0g.tar.gz > /dev/null
@@ -16,6 +23,15 @@ openssl aes-256-cbc -k "$ENCRYPTED_ANDROID_SECRETS_XML_PW" -in ./encrypted/secre
 openssl aes-256-cbc -k "$ENCRYPTED_ETSMOBILE_KEYSTORE_PW" -in ./encrypted/etsm_upload_ks.jks.encrypted -out ./etsm_upload_ks.jks -d
 openssl aes-256-cbc -k "$ENCRYPTED_KEYSTORE_PROPERTIES_PW" -in ./encrypted/keystore.properties.encrypted -out ./keystore.properties -d
 openssl aes-256-cbc -k "$ENCRYPTED_SERVICE_ACCOUNT_CREDENTIALS_BETA_PW" -in ./encrypted/service_account_credentials_beta.json.encrypted -out ./service_account_credentials_beta.json -d
+
+if ! dpkg -s openjdk-8-jdk >/dev/null 2>&1; then sudo apt-get update; sudo apt-get install openjdk-8-jdk; fi
+
+# download und unzip Android SDK Tools
+wget https://dl.google.com/android/repository/$ANDROID_SDK_TOOLS
+unzip -q $ANDROID_SDK_TOOLS -d $HOME/sdk
+# create empty cfg file to prevent sdkmanager warning message
+mkdir -p $HOME/.android && touch $HOME/.android/repositories.cfg
+
 yes | sdkmanager "platform-tools" > /dev/null
 yes | sdkmanager "build-tools;28.0.2" > /dev/null
 yes | sdkmanager "platforms;android-28" > /dev/null
