89fa23d9a445c2331ff5018ac2f2079da8781af1
zaclimon <isaacpateau05@gmail.com>
2019-04-20 09:47:24 -0400

    Optimize Android Travis build process based on previous expriments
    A Travis-CI build might take at least 25 minutes, this in itself
    might not be the best solution available. Because of that, let's
    optimize the build flow in an attempt to reduce build times
    
    - Install all the required Android SDK components into before_install.sh
    - Install the canary version (29.0.3) of the emulator which supports x86
    images without acceleration. (Travis doesn't support nested virtualization)
    - Move the emulator creation/start-up into the install section of
    travis.yml
    
    More info about the emulator optimization over here:
    https://travis-ci.community/t/android-emulators-not-starting-for-the-last-few-days-late-march-2019/2871/11

--- android/scripts/before_install.sh
+++ android/scripts/before_install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 lsb_release -a
 wget https://www.openssl.org/source/openssl-1.1.0g.tar.gz > /dev/null
 tar xzvf openssl-1.1.0g.tar.gz > /dev/null
@@ -15,9 +16,10 @@ openssl aes-256-cbc -k "$ENCRYPTED_ANDROID_SECRETS_XML_PW" -in ./encrypted/secre
 openssl aes-256-cbc -k "$ENCRYPTED_ETSMOBILE_KEYSTORE_PW" -in ./encrypted/etsm_upload_ks.jks.encrypted -out ./etsm_upload_ks.jks -d
 openssl aes-256-cbc -k "$ENCRYPTED_KEYSTORE_PROPERTIES_PW" -in ./encrypted/keystore.properties.encrypted -out ./keystore.properties -d
 openssl aes-256-cbc -k "$ENCRYPTED_SERVICE_ACCOUNT_CREDENTIALS_BETA_PW" -in ./encrypted/service_account_credentials_beta.json.encrypted -out ./service_account_credentials_beta.json -d
+yes | sdkmanager "platform-tools" > /dev/null
+yes | sdkmanager "build-tools;28.0.2" > /dev/null
 yes | sdkmanager "platforms;android-28" > /dev/null
-yes | sdkmanager "platforms;android-27" > /dev/null
-echo no | android create avd --force -n test -t android-21 --abi armeabi-v7a
-emulator -avd test -no-audio -no-window &
-android-wait-for-emulator
-adb shell input keyevent 82 &
+yes | sdkmanager "extras;google;m2repository" > /dev/null
+yes | sdkmanager "extras;android;m2repository" > /dev/null
+yes | sdkmanager "system-images;android-21;default;x86_64" > /dev/null
+yes | sdkmanager --channel=3 "emulator" > /dev/null
