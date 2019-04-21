8e42725fea5b6a9781b049a054b205e7976c2ffc
zaclimon <isaacpateau05@gmail.com>
2019-04-21 13:31:45 -0400

    build-check.sh: Only execute "git diff" once

--- .build-check.sh
+++ .build-check.sh
@@ -4,10 +4,12 @@
 #
 # Club App|ETS
 
-if [[ ! $(git diff --name-only "$TRAVIS_COMMIT_RANGE") =~ "android/" && "$TRAVIS_OS_NAME" = "linux" ]] || 
-   [[ ! $(git diff --name-only "$TRAVIS_COMMIT_RANGE") =~ "ios/" && "$TRAVIS_OS_NAME" = "osx" ]]; then
+MODIFIED_FILES=$(git diff --name-only "$TRAVIS_COMMIT_RANGE")
+
+if [[ ! "$MODIFIED_FILES" =~ "android/" && "$TRAVIS_OS_NAME" = "linux" ]] || 
+   [[ ! "$MODIFIED_FILES" =~ "ios/" && "$TRAVIS_OS_NAME" = "osx" ]]; then
     # It might be possible that travis.yml has been modified. In that case, continue through the build.
-    if [[ ! $(git diff --name-only "$TRAVIS_COMMIT_RANGE") =~ ".travis.yml" ]]; then
+    if [[ ! "$MODIFIED_FILES" =~ ".travis.yml" ]]; then
         echo "No project files or travis.yml modified. Exiting."
         exit 1
     fi
