b209a8236266bfbf7a1f91ca85e56babb325e5cd
Isaac Pateau <isaac_pateau@hotmail.com>
2019-04-21 20:14:10 -0400

    Optimisation du processus d'intégration continue avec Travis CI (#94)
    * Optimize Android Travis build process based on previous expriments
    
    A Travis-CI build might take at least 25 minutes, this in itself
    might not be the best solution available. Because of that, let's
    optimize the build flow in an attempt to reduce build times
    
    - Install all the required Android SDK components into before_install.sh
    - Install the canary version (29.0.3) of the emulator which supports x86
    images without acceleration. (Travis doesn't support nested virtualization)
    - Move the emulator creation/start-up into the install section of
    travis.yml
    
    More info about the emulator optimization over here:
    https://travis-ci.community/t/android-emulators-not-starting-for-the-last-few-days-late-march-2019/2871/11
    
    * travis.yml: Set correctly the envrionment variables
    
    Since the Travis-CI image for Android is a bit outdated, adjust
    PATH so it can be used with the newer executables from the SDK.
    
    Based on:
    https://travis-ci.org/ankidroid/Anki-Android/jobs/516486600/config
    
    * travis.yml: Use debug version when building/testing
    
    * travis.yml: Remove sudo: false attribute
    
    More info:
    https://docs.travis-ci.com/user/reference/overview/#deprecated-virtualization-environments
    
    * travis.yml: Remove Gradle locks before cache
    
    * Adapt build for Ubuntu Xenial (16.04)
    
    - Change the OS to Xenial with minimal language in travis.yml
    - Download and extract the Android SDK manually
    - Change the PATH accordingly in travis.yml
    - Downgrade the version of JDK used to JDK8 (A more recent version is used)
    - Add android-wait-for-emulator script for waiting on a booted instance
    of the Android emulator for instrumentation tests
    
    Configuration snippets based from:
    https://travis-ci.com/G00fY2/android-ci-testproject/jobs/187085499/config
    
    * Add some documentation for Travis related stuff
    
    * Build only if the project files have been changed
    
    Only build projects if actual code has changed. Since there are 2
    projects in one repo. One project might always get built without
    any seen changes (For example an iOS commit triggers an Android build)
    
    For this reason, cancel a build only if the related commits
    (Eg. From a PR) isn't affecting the other's project.
    
    Don't apply this rule for travis.yml which might be used for testing
    Travis-CI infrastructure.
    
    * Add executable permission to Travis-CI scripts
    
    * Use variable expansion for checking the build
    
    * build-check.sh: Only execute "git diff" once
    
    * travis.yml: Little clean up
    
    * Transfer Android beta deployment process to travis.yml
    
    Since the keys are already decrypted in the before_install step,
    it is not necessary to decrypt them again.
    
    In that case, we only have a few commands and thus, they should be executed
    directly in the .yml file.
    
    * Android: Remove unused variable on build_install.sh
    
    * travis.yml: Rename ETS-mobile paths to ETSMobile for iOS tests
    
    * .travis.yml: Correctly specify the OS and the language

---
+++ .build-check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Modified files check script
+# Terminates Travis-CI builds whether project related files hasn't been modified
+#
+# Club App|ETS
+
+MODIFIED_FILES=$(git diff --name-only "$TRAVIS_COMMIT_RANGE")
+
+if [[ ! "$MODIFIED_FILES" =~ "android/" && "$TRAVIS_OS_NAME" = "linux" ]] || 
+   [[ ! "$MODIFIED_FILES" =~ "ios/" && "$TRAVIS_OS_NAME" = "osx" ]]; then
+    # It might be possible that travis.yml has been modified. In that case, continue through the build.
+    if [[ ! "$MODIFIED_FILES" =~ ".travis.yml" ]]; then
+        echo "No project files or travis.yml modified. Exiting."
+        exit 1
+    fi
+fi
--- android/scripts/before_install.sh
+++ android/scripts/before_install.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+# Android SDK configuration script for ÉTSMobile on Travis-CI
+# Running on Ubuntu 16.04 (Xenial Xerus)
+# Club App|ETS
+
+# SDK Tools Revision 26.1.1
+# For Updates check https://developer.android.com/studio#downloads
+ANDROID_SDK_TOOLS=sdk-tools-linux-4333796.zip
+
+# Compile and install OpenSSL
 lsb_release -a
 wget https://www.openssl.org/source/openssl-1.1.0g.tar.gz > /dev/null
 tar xzvf openssl-1.1.0g.tar.gz > /dev/null
@@ -8,6 +18,8 @@ sudo make install > /dev/null
 export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib64
 sudo ldconfig
 pwd
+
+# Decrypt secrets for potential deployments
 cd ../android
 openssl version -v
 openssl aes-256-cbc -k "$ENCRYPTED_ANDROID_GOOGLE_SERVICES_PW" -in ./encrypted/google-services.json.encrypted -out ./app/google-services.json -d
@@ -15,9 +27,21 @@ openssl aes-256-cbc -k "$ENCRYPTED_ANDROID_SECRETS_XML_PW" -in ./encrypted/secre
 openssl aes-256-cbc -k "$ENCRYPTED_ETSMOBILE_KEYSTORE_PW" -in ./encrypted/etsm_upload_ks.jks.encrypted -out ./etsm_upload_ks.jks -d
 openssl aes-256-cbc -k "$ENCRYPTED_KEYSTORE_PROPERTIES_PW" -in ./encrypted/keystore.properties.encrypted -out ./keystore.properties -d
 openssl aes-256-cbc -k "$ENCRYPTED_SERVICE_ACCOUNT_CREDENTIALS_BETA_PW" -in ./encrypted/service_account_credentials_beta.json.encrypted -out ./service_account_credentials_beta.json -d
+
+# Install OpenJDK 8 for compiling the application
+if ! dpkg -s openjdk-8-jdk >/dev/null 2>&1; then sudo apt-get update; sudo apt-get install openjdk-8-jdk; fi
+
+# Download und unzip Android SDK Tools
+wget https://dl.google.com/android/repository/$ANDROID_SDK_TOOLS
+unzip -q $ANDROID_SDK_TOOLS -d $HOME/sdk
+# Create empty cfg file to prevent sdkmanager warning message
+mkdir -p $HOME/.android && touch $HOME/.android/repositories.cfg
+
+# Install Android SDK components
+yes | sdkmanager "platform-tools" > /dev/null
+yes | sdkmanager "build-tools;28.0.2" > /dev/null
 yes | sdkmanager "platforms;android-28" > /dev/null
-yes | sdkmanager "platforms;android-27" > /dev/null
-echo no | android create avd --force -n test -t android-21 --abi armeabi-v7a
-emulator -avd test -no-audio -no-window &
-android-wait-for-emulator
-adb shell input keyevent 82 &
+yes | sdkmanager "extras;google;m2repository" > /dev/null
+yes | sdkmanager "extras;android;m2repository" > /dev/null
+yes | sdkmanager "system-images;android-21;default;x86_64" > /dev/null
+yes | sdkmanager --channel=3 "emulator" > /dev/null
