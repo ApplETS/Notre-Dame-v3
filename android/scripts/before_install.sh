98efb14bef1ecd777b49b0955b5351279e2d94cd
zaclimon <isaacpateau05@gmail.com>
2019-04-22 13:55:37 -0400

    Remove secrets decryption in before_install for Android
    Since we are compiling the debug version of the app, it is not necessary
    to have release related files.

--- android/scripts/before_install.sh
+++ android/scripts/before_install.sh
@@ -19,15 +19,6 @@ export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib64
 sudo ldconfig
 pwd
 
-# Decrypt secrets for potential deployments
-cd ../android
-openssl version -v
-openssl aes-256-cbc -k "$ENCRYPTED_ANDROID_GOOGLE_SERVICES_PW" -in ./encrypted/google-services.json.encrypted -out ./app/google-services.json -d
-openssl aes-256-cbc -k "$ENCRYPTED_ANDROID_SECRETS_XML_PW" -in ./encrypted/secrets.xml.encrypted -out ./app/src/main/res/values/secrets.xml -d
-openssl aes-256-cbc -k "$ENCRYPTED_ETSMOBILE_KEYSTORE_PW" -in ./encrypted/etsm_upload_ks.jks.encrypted -out ./etsm_upload_ks.jks -d
-openssl aes-256-cbc -k "$ENCRYPTED_KEYSTORE_PROPERTIES_PW" -in ./encrypted/keystore.properties.encrypted -out ./keystore.properties -d
-openssl aes-256-cbc -k "$ENCRYPTED_SERVICE_ACCOUNT_CREDENTIALS_BETA_PW" -in ./encrypted/service_account_credentials_beta.json.encrypted -out ./service_account_credentials_beta.json -d
-
 # Install OpenJDK 8 for compiling the application
 if ! dpkg -s openjdk-8-jdk >/dev/null 2>&1; then sudo apt-get update; sudo apt-get install openjdk-8-jdk; fi
 
