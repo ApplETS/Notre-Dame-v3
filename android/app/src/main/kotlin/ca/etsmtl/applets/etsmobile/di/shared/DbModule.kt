3d0df81f682104e93ec47bb6aceacab802985af0
Thanh-Son-Philippe Lam <t.s.philippe_lam@hotmail.com>
2019-06-03 01:53:08 -0400

    Use Binds annotation instead of Provided to inject dep
    Binds is more efficient

--- android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/di/shared/DbModule.kt
+++ android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/di/shared/DbModule.kt
@@ -1,24 +1,21 @@
 package ca.etsmtl.applets.etsmobile.di.shared
 
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import data.db.DashboardCardDatabase
 import data.db.DashboardCardDatabaseImpl
-import javax.inject.Singleton
 
 /**
  * Created by Sonphil on 09-02-19.
  */
 
 @Module(includes = [DbModule.Providers::class])
-internal object DbModule {
+internal abstract class DbModule {
+    @Binds
+    abstract fun provideDashboardCardDatabase(impl: DashboardCardDatabaseImpl): DashboardCardDatabase
+
     @Module
     internal object Providers {
-        @JvmStatic
-        @Provides
-        @Singleton
-        fun provideDashboardCardDatabase(): DashboardCardDatabase {
-            return DashboardCardDatabaseImpl()
-        }
+
     }
 }
