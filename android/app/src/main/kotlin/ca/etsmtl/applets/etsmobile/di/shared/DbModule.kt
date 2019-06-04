19e9774948b91bfcd34e4c825d6fc1304e63cafb
Thanh-Son-Philippe Lam <t.s.philippe_lam@hotmail.com>
2019-06-04 04:23:57 -0400

    Implement DashboardCardDatabase with Room
    rxxjava support for Room has been added because it doesn't have ReceiveChannel yet. However, we need to observe for changes in the DB. To do, we convert the Flowable into a ReceiveChannel.

--- android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/di/shared/DbModule.kt
+++ android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/di/shared/DbModule.kt
@@ -1,9 +1,23 @@
 package ca.etsmtl.applets.etsmobile.di.shared
 
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
+import data.db.AppRoomDatabase
 import data.db.DashboardCardDatabase
 import data.db.DashboardCardDatabaseImpl
+import data.db.dao.DashboardCardDao
+import data.db.entity.DashboardCardEntity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import model.DashboardCard
+import utils.EtsMobileDispatchers
+import javax.inject.Singleton
 
 /**
  * Created by Sonphil on 09-02-19.
@@ -11,11 +25,56 @@ import data.db.DashboardCardDatabaseImpl
 
 @Module(includes = [DbModule.Providers::class])
 internal abstract class DbModule {
-    @Binds
-    abstract fun provideDashboardCardDatabase(impl: DashboardCardDatabaseImpl): DashboardCardDatabase
-
     @Module
     internal object Providers {
+        private lateinit var DB_INSTANCE: AppRoomDatabase
 
+        @JvmStatic
+        @Singleton
+        @Provides
+        fun provideDb(context: Context): AppRoomDatabase {
+            DB_INSTANCE = Room.databaseBuilder(context, AppRoomDatabase::class.java, "etsmobileshared.db")
+                .fallbackToDestructiveMigration()
+                .addCallback(object : RoomDatabase.Callback() {
+                    override fun onCreate(db: SupportSQLiteDatabase) {
+                        initDatabaseContent(DB_INSTANCE)
+                    }
+                })
+                .build()
+
+            return DB_INSTANCE
+        }
+
+        private fun initDatabaseContent(db: AppRoomDatabase) {
+            CoroutineScope(EtsMobileDispatchers.IO).launch {
+                runBlocking {
+                    db.dashboardCardDao().let { dao ->
+                        dao.insertDashboardCard(DashboardCardEntity(
+                            DashboardCard.Type.DASHBOARD_CARD_APPLETS.name,
+                            true,
+                            DashboardCard.Type.DASHBOARD_CARD_APPLETS.ordinal
+                        ))
+                        dao.insertDashboardCard(DashboardCardEntity(
+                            DashboardCard.Type.DASHBOARD_CARD_TODAY_SCHEDULE.name,
+                            true,
+                            DashboardCard.Type.DASHBOARD_CARD_TODAY_SCHEDULE.ordinal
+                        ))
+                        dao.insertDashboardCard(DashboardCardEntity(
+                            DashboardCard.Type.DASHBOARD_CARD_GRADES.name,
+                            true,
+                            DashboardCard.Type.DASHBOARD_CARD_GRADES.ordinal
+                        ))
+                    }
+                }
+            }
+        }
+
+        @JvmStatic
+        @Singleton
+        @Provides
+        fun provideDashboardCardDao(db: AppRoomDatabase): DashboardCardDao = db.dashboardCardDao()
     }
+
+    @Binds
+    abstract fun provideDashboardCardDatabase(impl: DashboardCardDatabaseImpl): DashboardCardDatabase
 }
--- shared/src/androidMain/kotlin/data/db/DashboardCardDatabaseImpl.kt
+++ shared/src/androidMain/kotlin/data/db/DashboardCardDatabaseImpl.kt
@@ -1,6 +1,12 @@
 package data.db
 
-import kotlinx.coroutines.channels.Channel
+import data.db.dao.DashboardCardDao
+import data.db.entity.DashboardCardEntity
+import data.db.entity.mapper.toDashboardCardEntity
+import data.db.entity.mapper.toDashboardCards
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.reactive.openSubscription
+import kotlinx.coroutines.runBlocking
 import model.DashboardCard
 import javax.inject.Inject
 
@@ -8,16 +14,36 @@ import javax.inject.Inject
  * Created by Sonphil on 02-06-19.
  */
 
-class DashboardCardDatabaseImpl @Inject constructor(): DashboardCardDatabase {
-    override suspend fun dashboardCards(): Channel<List<DashboardCard>> {
-        TODO()
+class DashboardCardDatabaseImpl @Inject constructor(
+    private val dao: DashboardCardDao
+) : DashboardCardDatabase {
+    override suspend fun dashboardCards(): ReceiveChannel<List<DashboardCard>> {
+        return dao.getAll().map {
+            it.toDashboardCards()
+        }.openSubscription()
     }
 
     override suspend fun updateCard(dashboardCard: DashboardCard, position: Int) {
-        TODO()
+        dao.updateDashboardCard(dashboardCard.toDashboardCardEntity(position))
     }
 
     override suspend fun reset() {
-        TODO()
-    }
+        runBlocking {
+            dao.deleteAll()
+            dao.insertDashboardCard(DashboardCardEntity(
+                DashboardCard.Type.DASHBOARD_CARD_APPLETS.name,
+                true,
+                DashboardCard.Type.DASHBOARD_CARD_APPLETS.ordinal
+            ))
+            dao.insertDashboardCard(DashboardCardEntity(
+                DashboardCard.Type.DASHBOARD_CARD_TODAY_SCHEDULE.name,
+                true,
+                DashboardCard.Type.DASHBOARD_CARD_TODAY_SCHEDULE.ordinal
+            ))
+            dao.insertDashboardCard(DashboardCardEntity(
+                DashboardCard.Type.DASHBOARD_CARD_GRADES.name,
+                true,
+                DashboardCard.Type.DASHBOARD_CARD_GRADES.ordinal
+            ))}
+        }
 }
