68c2cd11b8f90aab44895efb291f7db0f61f8b23
Thanh-Son-Philippe Lam <t.s.philippe_lam@hotmail.com>
2019-06-15 12:14:53 -0400

    [skip travis] Remove SQLDelight and klock (#157)
    * Include shared folder in build.check.sh
    
    The build should run if the shared module has been modified.
    
    * Remove klock and add our own implementation of Date (no native implementation yet)
    
    Kotlin doesn't have one yet and there are some issues with the klock library at the moment.
    
    * Retrait de SQLDelight (#152)
    
    * Retrait de SQLDelight
    
    Tel que discuté, en raison des problèmes rencontrés avec SQLDelight, on va retirer la librairie et avoir une implémentation pour chaque plateforme. Chaque plateforme va implémenter l'interface `DashboardCardDatabase` (TODO pour l'instant). Ce dernier constitue une dépendance pour la classe `DashboardCardRepository`.
    
    * Fix tests
    
    - Bump mockito version
    - Use Coroutines for logging out
    
    * Use Binds annotation instead of Provided to inject dep
    
    Binds is more efficient
    
    * Fix DashboardViewModel transmitting only one value
    
    * Implement DashboardCardDatabase with Room
    
    rxxjava support for Room has been added because it doesn't have ReceiveChannel yet. However, we need to observe for changes in the DB. To do, we convert the Flowable into a ReceiveChannel.
    
    * Use a separate DB for the dashboard cards
    
    * Exclude 'META-INF/*.kotlin_module' in packagingOptions

---
+++ shared/src/androidMain/kotlin/data/db/entity/mapper/DashboardCardMapperExt.kt
@@ -0,0 +1,23 @@
+package data.db.entity.mapper
+
+import data.db.entity.DashboardCardEntity
+import model.DashboardCard
+
+/**
+ * Created by Sonphil on 04-06-19.
+ */
+ 
+fun DashboardCardEntity.toDashboardCard() = DashboardCard(
+    DashboardCard.Type.valueOf(type),
+    visible,
+    dismissible
+)
+
+fun List<DashboardCardEntity>.toDashboardCards() = map { it.toDashboardCard() }
+
+fun DashboardCard.toDashboardCardEntity(position: Int) = DashboardCardEntity(
+    type.name,
+    visible,
+    position,
+    dismissible
+)
