8a94e5ee54f56bbd2c7a76a17e52fcd4bda91efb
Thanh-Son-Philippe Lam <t.s.philippe_lam@hotmail.com>
2019-06-15 12:14:53 -0400

    [skip travis] Remove SQLDelight and klock (#157)
    * Include shared folder in build.check.sh
    
    The build should run if the shared module has been modified.
    
    * Remove klock and add our own implementation of Date (no native implementation yet)
    
    Kotlin doesn't have one yet and there are some issues with the klock library at the moment.
    
    * Retrait de SQLDelight (#152)
    
    * Retrait de SQLDelight
    
    Tel que discuté, en raison des problèmes rencontrés avec SQLDelight, on va retirer la librairie et avoir une implémentation pour chaque plateforme. Chaque plateforme va implémenter l'interface `DashboardCardDatabase` (TODO pour l'instant). Ce dernier constitue une dépendance pour la classe `DashboardCardRepository`.
    
    * Fix tests
    
    - Bump mockito version
    - Use Coroutines for logging out
    
    * Use Binds annotation instead of Provided to inject dep
    
    Binds is more efficient
    
    * Fix DashboardViewModel transmitting only one value
    
    * Implement DashboardCardDatabase with Room
    
    rxxjava support for Room has been added because it doesn't have ReceiveChannel yet. However, we need to observe for changes in the DB. To do, we convert the Flowable into a ReceiveChannel.
    
    * Use a separate DB for the dashboard cards
    
    * Exclude 'META-INF/*.kotlin_module' in packagingOptions

--- shared/src/commonMain/kotlin/data/repository/DashboardCardRepository.kt
+++ shared/src/commonMain/kotlin/data/repository/DashboardCardRepository.kt
@@ -1,12 +1,8 @@
 package data.repository
 
-import ca.etsmtl.applets.shared.db.DashboardCardQueries
+import data.db.DashboardCardDatabase
 import di.Inject
-import extension.asChannel
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.ReceiveChannel
-import kotlinx.coroutines.channels.map
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import model.DashboardCard
 import utils.EtsMobileDispatchers
@@ -15,27 +11,20 @@ import utils.EtsMobileDispatchers
  * Created by Sonphil on 09-02-19.
  */
 
-class DashboardCardRepository @Inject constructor(private val dashboardCardQueries: DashboardCardQueries) {
+class DashboardCardRepository @Inject constructor(private val database: DashboardCardDatabase) {
     suspend fun dashboardCards(): ReceiveChannel<List<DashboardCard>> {
         return withContext(EtsMobileDispatchers.IO) {
-            dashboardCardQueries
-                .selectAll { type, _, visible, dismissible ->
-                    DashboardCard(type, visible, dismissible)
-                }
-                .asChannel()
-                .map(EtsMobileDispatchers.IO) { query ->
-                    query.executeAsList()
-                }
+            database.dashboardCards()
         }
     }
 
-    fun updateDashboardCard(card: DashboardCard, position: Int) = CoroutineScope(EtsMobileDispatchers.IO)
-        .launch {
-            dashboardCardQueries.updateCard(position.toLong(), card.visible, card.type)
+    suspend fun updateDashboardCard(card: DashboardCard, position: Int) {
+        withContext(EtsMobileDispatchers.IO) {
+            database.updateCard(card, position)
         }
+    }
 
-    fun restore() {
-        dashboardCardQueries.deleteAll()
-        dashboardCardQueries.insertInitialCards()
+    suspend fun restore() = withContext(EtsMobileDispatchers.IO) {
+        database.reset()
     }
 }
