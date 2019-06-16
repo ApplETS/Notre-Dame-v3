ae8a439c93867c2e5914e8ae49af43509dfbf75c
Thanh-Son-Philippe Lam <t.s.philippe_lam@hotmail.com>
2019-06-16 14:08:27 -0400

    Use ktor to implement HTTP client

---
+++ shared/src/commonMain/kotlin/data/api/MonETSApi.kt
@@ -0,0 +1,28 @@
+package data.api
+
+import data.api.model.MonETSAuthenticationRequestBody
+import data.api.model.MonETSUser
+import di.Inject
+import io.ktor.client.HttpClient
+import io.ktor.client.request.post
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.http.takeFrom
+
+/**
+ * Created by Sonphil on 18-05-19.
+ */
+
+class MonETSApi @Inject constructor(private val client: HttpClient = AppHttpClient.httpClient) {
+    companion object {
+        private const val BASE_URL = "https://portail.etsmtl.ca/api/"
+    }
+
+    suspend fun authenticateUser(requestBody: MonETSAuthenticationRequestBody): MonETSUser = client.post {
+        url {
+            takeFrom(BASE_URL + "authentification")
+        }
+        contentType(ContentType.Application.Json)
+        body = requestBody
+    }
+}
