0b18ed9f7b883188663411f91a5f1493fa68adca
May Madany <may-mohamed-abo.madany.1@ens.etsmtl.ca>
2019-07-09 14:00:44 -0400

    adding internet conncted in values.xml

--- android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/presentation/main/MainActivity.kt
+++ android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/presentation/main/MainActivity.kt
@@ -17,7 +17,6 @@ import androidx.navigation.ui.setupWithNavController
 import ca.etsmtl.applets.etsmobile.R
 import ca.etsmtl.applets.etsmobile.extension.getColorCompat
 import ca.etsmtl.applets.etsmobile.extension.setVisible
-import ca.etsmtl.applets.etsmobile.presentation.App
 import ca.etsmtl.applets.etsmobile.presentation.BaseActivity
 import ca.etsmtl.applets.etsmobile.util.EventObserver
 import com.google.android.material.bottomnavigation.BottomNavigationItemView
