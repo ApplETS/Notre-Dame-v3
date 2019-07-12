07e37f18fc405b9ce6cf07baacc796d8d2411112
Thanh-Son-Philippe Lam <t.s.philippe_lam@hotmail.com>
2019-07-12 18:22:03 -0400

    Use expect/actual mechanism for DashboardCardDatabase (#170)

--- shared/src/iosMain/kotlin/data/db/DashboardCardDatabase.kt
+++ shared/src/iosMain/kotlin/data/db/DashboardCardDatabase.kt
@@ -7,19 +7,25 @@ import model.DashboardCard
  * Created by Sonphil on 02-06-19.
  */
 
-expect class DashboardCardDatabase {
+actual class DashboardCardDatabase {
     /**
      * Returns the dashboard cards ordered according the user preference
      */
-    fun dashboardCards(): ReceiveChannel<List<DashboardCard>>
+    actual fun dashboardCards(): ReceiveChannel<List<DashboardCard>> {
+        TODO("not implemented")
+    }
 
     /**
      * Update a dashboard card and set to the provided position
      */
-    suspend fun updateCard(dashboardCard: DashboardCard, position: Int)
+    actual suspend fun updateCard(dashboardCard: DashboardCard, position: Int) {
+        TODO("not implemented")
+    }
 
     /**
      * Restore the default cards
      */
-    suspend fun reset()
+    actual suspend fun reset() {
+        TODO("not implemented")
+    }
 }
