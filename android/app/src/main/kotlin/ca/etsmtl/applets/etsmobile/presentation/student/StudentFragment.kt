dcd371bc9a65ceca4bc7f8d8975cb5544559be47
Thanh-Son-Philippe Lam <t.s.philippe_lam@hotmail.com>
2019-07-28 17:32:40 -0400

    Fix issue causing the `ProgressBar` to be always shown when the device was offline (#180)
    Refactor `FetchCurrentAndFutureSeancesUseCase`

--- android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/presentation/student/StudentFragment.kt
+++ android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/presentation/student/StudentFragment.kt
@@ -5,14 +5,12 @@ import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.os.postDelayed
 import androidx.core.view.isVisible
 import ca.etsmtl.applets.etsmobile.R
 import ca.etsmtl.applets.etsmobile.extension.fadeTo
 import ca.etsmtl.applets.etsmobile.presentation.main.MainActivity
 import com.google.android.material.tabs.TabLayout
 import dagger.android.support.DaggerFragment
-import kotlinx.android.synthetic.main.activity_main.appBarLayout
 import kotlinx.android.synthetic.main.activity_main.tabLayout
 import kotlinx.android.synthetic.main.fragment_student.viewPagerStudent
 
@@ -57,7 +55,6 @@ class StudentFragment : DaggerFragment() {
 
     override fun onDestroyView() {
         (activity as? MainActivity)?.let {
-            it.appBarLayout.setExpanded(true, false)
             it.tabLayout.setupWithViewPager(null)
             it.tabLayout.isVisible = false
         }
