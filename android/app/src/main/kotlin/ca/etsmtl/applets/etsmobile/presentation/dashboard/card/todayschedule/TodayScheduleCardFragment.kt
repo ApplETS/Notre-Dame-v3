4f8e9c3f35e3b7b315a2cb219a55beb1271cea64
Thanh-Son-Philippe Lam <t.s.philippe_lam@hotmail.com>
2019-07-30 16:42:23 -0400

    [skip travis] Add login with MonETS (#181)
    * Add ktor
    
    * Use ktor to implement HTTP client
    
    * Add LoginRepository to shared module
    
    * Add CheckUserCredentialsAreValidUseCase
    
    * Add FetchSavedUserCredentialsUseCase
    
    * Add LoginWithSavedCredentials
    
    * Add LoginViewModel
    
    * Move old LoginViewModel to splash directory
    
    It's still used for the splash screen.
    
    * Use shared LoginViewModel in LoginFragment
    
    * Add SplashViewModel
    
    * Add ClearUserDataUseCase to shared module
    
    * Remove old files
    
    * Clear shared preferences on logout
    
    * Use TextInputLayout's endIcon properties
    
    * Fix field error message not dismissed when no error
    
    * Fix error message not shown
    
    * Fix app bar layout expanded on login screen
    
    * Add localized strings to `shared` module
    
    Provides a temp solution to shared localized strings between the two platforms
    
    * Add LastValueReceiveChannel to keep last value
    
    For now, the class is only used to keep the last value of universal code dialog visibility. On Android, when a configuration change occurs, the Android ViewModel will keeps its data. However, as we simply use Channels and converted them into LiveDatas, the last values of the Channels weren't not getting saved. Before, when we didn't use Kotlin MP, we were declaring the LiveData inside the ViewModel and last value would be kept. When the observers would be active again, the LiveData would dispatch the last value. Channels don't have this feature since they are don't hold the data when it has been consumed. The problem is noticeable especially with the dialog. When the user rotated the screen, it would not reappear again. To fix this, we add a class called LastValueReceiveChannel that will keep the last value submitted and use a Channel by delegation.
    
    * Use LastValueReceiveChannel with field error message and add some comments
    
    Before, when the user changed the language on its device, the language of the message would not change.
    
    * Remove Event usage in LoginFragment
    
    Not needed since Channels don't keep the last value
    
    * Remove Event and EventObserver class from shared module
    
    * Rename `LoginWithSavedCredentials`to `LoginWithSavedCredentialsUseCase`

--- android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/presentation/dashboard/card/todayschedule/TodayScheduleCardFragment.kt
+++ android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/presentation/dashboard/card/todayschedule/TodayScheduleCardFragment.kt
@@ -72,8 +72,9 @@ class TodayScheduleCardFragment : DaggerFragment() {
 
         todayScheduleCardViewModel.errorMessage.observe(this, EventObserver {
             it?.let {
-                if(!it.equals(context?.getString(R.string.error_no_internet_connection)))
-                Toast.makeText(context, it, Toast.LENGTH_LONG).show() }
+                if (!it.equals(context?.getString(R.string.error_no_internet_connection)))
+                    Toast.makeText(context, it, Toast.LENGTH_LONG).show()
+            }
         })
 
         todayScheduleCardViewModel.loading.observe(this, Observer { visible ->
--- android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/presentation/splash/SplashFragment.kt
+++ android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/presentation/splash/SplashFragment.kt
@@ -11,11 +11,11 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import ca.etsmtl.applets.etsmobile.R
+import ca.etsmtl.applets.etsmobile.extension.toLiveData
 import ca.etsmtl.applets.etsmobile.extension.toast
-import ca.etsmtl.applets.etsmobile.presentation.login.LoginViewModel
-import ca.etsmtl.applets.etsmobile.util.EventObserver
 import dagger.android.support.DaggerFragment
 import kotlinx.android.synthetic.main.fragment_splash.progressBarSplash
+import presentation.SplashViewModel
 import javax.inject.Inject
 
 /**
@@ -23,8 +23,8 @@ import javax.inject.Inject
  */
 
 class SplashFragment : DaggerFragment() {
-    private val loginViewModel: LoginViewModel by lazy {
-        ViewModelProviders.of(this, viewModelFactory).get(LoginViewModel::class.java)
+    private val splashViewModel: SplashViewModel by lazy {
+        ViewModelProviders.of(this, viewModelFactory).get(SplashViewModel::class.java)
     }
 
     @Inject
@@ -40,28 +40,26 @@ class SplashFragment : DaggerFragment() {
         super.onViewCreated(view, savedInstanceState)
 
         subscribeUI()
+        splashViewModel.submitSavedCredentials()
     }
 
     private fun subscribeUI() {
-        with(loginViewModel) {
-            showLoading.observe(this@SplashFragment, Observer {
+        with(splashViewModel) {
+            showLoading.toLiveData().observe(this@SplashFragment, Observer {
                 progressBarSplash.isVisible = it == true
             })
-
-            errorMessage.observe(this@SplashFragment, EventObserver {
-                if(!it.equals(context?.getString(R.string.error_no_internet_connection)))
-                context?.toast(it, Toast.LENGTH_LONG)
+            errorMessage.toLiveData().observe(this@SplashFragment, Observer {
+                if (!it.equals(context?.getString(R.string.error_no_internet_connection)))
+                    context?.toast(it, Toast.LENGTH_LONG)
             })
 
-            navigateToLogin.observe(this@SplashFragment, EventObserver {
+            navigateToLogin.toLiveData().observe(this@SplashFragment, Observer {
                 findNavController().navigate(SplashFragmentDirections.actionFragmentSplashToFragmentLogin())
             })
 
-            navigateToDashboard.observe(this@SplashFragment, EventObserver {
+            navigateToDashboard.toLiveData().observe(this@SplashFragment, Observer {
                 findNavController().navigate(SplashFragmentDirections.actionFragmentSplashToFragmentDashboard())
             })
-
-            lifecycle.addObserver(this)
         }
     }
 
