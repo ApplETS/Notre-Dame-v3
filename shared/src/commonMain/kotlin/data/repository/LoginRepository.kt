977699bd6741ec29b37276ade87c7e844e1fba0d
Thanh-Son-Philippe Lam <t.s.philippe_lam@hotmail.com>
2019-07-30 16:42:23 -0400

    [skip travis] Add login with MonETS (#181)
    * Add ktor
    
    * Use ktor to implement HTTP client
    
    * Add LoginRepository to shared module
    
    * Add CheckUserCredentialsAreValidUseCase
    
    * Add FetchSavedUserCredentialsUseCase
    
    * Add LoginWithSavedCredentials
    
    * Add LoginViewModel
    
    * Move old LoginViewModel to splash directory
    
    It's still used for the splash screen.
    
    * Use shared LoginViewModel in LoginFragment
    
    * Add SplashViewModel
    
    * Add ClearUserDataUseCase to shared module
    
    * Remove old files
    
    * Clear shared preferences on logout
    
    * Use TextInputLayout's endIcon properties
    
    * Fix field error message not dismissed when no error
    
    * Fix error message not shown
    
    * Fix app bar layout expanded on login screen
    
    * Add localized strings to `shared` module
    
    Provides a temp solution to shared localized strings between the two platforms
    
    * Add LastValueReceiveChannel to keep last value
    
    For now, the class is only used to keep the last value of universal code dialog visibility. On Android, when a configuration change occurs, the Android ViewModel will keeps its data. However, as we simply use Channels and converted them into LiveDatas, the last values of the Channels weren't not getting saved. Before, when we didn't use Kotlin MP, we were declaring the LiveData inside the ViewModel and last value would be kept. When the observers would be active again, the LiveData would dispatch the last value. Channels don't have this feature since they are don't hold the data when it has been consumed. The problem is noticeable especially with the dialog. When the user rotated the screen, it would not reappear again. To fix this, we add a class called LastValueReceiveChannel that will keep the last value submitted and use a Channel by delegation.
    
    * Use LastValueReceiveChannel with field error message and add some comments
    
    Before, when the user changed the language on its device, the language of the message would not change.
    
    * Remove Event usage in LoginFragment
    
    Not needed since Channels don't keep the last value
    
    * Remove Event and EventObserver class from shared module
    
    * Rename `LoginWithSavedCredentials`to `LoginWithSavedCredentialsUseCase`

---
+++ shared/src/commonMain/kotlin/data/repository/LoginRepository.kt
@@ -0,0 +1,61 @@
+package data.repository
+
+import data.api.MonETSApi
+import data.api.model.MonETSAuthenticationRequestBody
+import data.api.model.MonETSUser
+import data.db.DashboardCardDatabase
+import data.securepreferences.SecurePreferences
+import di.Inject
+import kotlinx.coroutines.withContext
+import model.SignetsUserCredentials
+import model.UniversalCode
+import utils.EtsMobileDispatchers
+
+class LoginRepository @Inject constructor(
+    private val securePrefs: SecurePreferences,
+    private val monETSApi: MonETSApi,
+    private val dashboardCardDatabase: DashboardCardDatabase
+) {
+    companion object {
+        const val UNIVERSAL_CODE_KEY = "UniversalCodeKey"
+        const val PASSWORD_KEY = "PasswordKey"
+        const val DOMAINE_KEY = "DomaineKey"
+    }
+
+    fun getSavedCredentials(): SignetsUserCredentials? {
+        val universalCode = securePrefs.getString(UNIVERSAL_CODE_KEY, null)
+        val password = securePrefs.getString(PASSWORD_KEY, null)
+
+        return if (universalCode != null && password != null) {
+            SignetsUserCredentials.INSTANCE = SignetsUserCredentials(UniversalCode(universalCode), password)
+            SignetsUserCredentials.INSTANCE
+        } else {
+            null
+        }
+    }
+
+    suspend fun authenticate(creds: SignetsUserCredentials): MonETSUser = withContext(EtsMobileDispatchers.IO) {
+        val monETSUser = monETSApi.authenticateUser(MonETSAuthenticationRequestBody(
+            creds.motPasse,
+            creds.codeAccesUniversel.value
+        ))
+
+        securePrefs.putString(UNIVERSAL_CODE_KEY, creds.codeAccesUniversel.value)
+        securePrefs.putString(PASSWORD_KEY, creds.motPasse)
+        securePrefs.putString(DOMAINE_KEY, monETSUser.domaine)
+
+        SignetsUserCredentials.INSTANCE = creds
+
+        monETSUser
+    }
+
+    suspend fun clearUserData() {
+        withContext(EtsMobileDispatchers.IO) {
+            securePrefs.clear()
+
+            SignetsUserCredentials.INSTANCE = null
+
+            dashboardCardDatabase.reset()
+        }
+    }
+}
---
+++ shared/src/commonMain/kotlin/utils/LastValueReceiveChannel.kt
@@ -0,0 +1,24 @@
+package utils
+
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ReceiveChannel
+
+/**
+ * A channel that keeps its last value
+ *
+ * Created by Sonphil on 29-07-19.
+ */
+
+class LastValueReceiveChannel<T>(private val channel: Channel<T?> = Channel()) : ReceiveChannel<T?> by channel {
+    var lastValue: T? = null
+        private set
+
+    /**
+     * Save [element] into [lastValue] and send [element]
+     */
+    suspend fun submit(element: T?) {
+        lastValue = element
+
+        channel.send(element)
+    }
+}
