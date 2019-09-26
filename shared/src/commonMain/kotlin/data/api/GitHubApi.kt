163bf778a4758ce02327611d09d654574d62bea2
Thanh-Son-Philippe Lam <t.s.philippe_lam@hotmail.com>
2019-09-26 16:20:45 -0400

    Add GitHubApi and GitHubContributor model class

---
+++ shared/src/commonMain/kotlin/data/api/GitHubApi.kt
@@ -0,0 +1,29 @@
+package data.api
+
+import data.api.model.GitHubContributor
+import di.Inject
+import io.ktor.client.request.accept
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import io.ktor.http.ContentType
+import io.ktor.http.takeFrom
+
+/**
+ * Created by Sonphil on 26-09-19.
+ */
+
+class GitHubApi @Inject constructor() {
+    private val client = AppHttpClient.httpClient
+
+    companion object {
+        private const val BASE_URL = "https://api.github.com/"
+    }
+
+    suspend fun contributors(): List<GitHubContributor> = client.get {
+        url {
+            takeFrom(BASE_URL + "repos/ApplETS/Notre-Dame/contributors")
+        }
+        parameter("anon", 0)
+        accept(ContentType.Application.Json)
+    }
+}
