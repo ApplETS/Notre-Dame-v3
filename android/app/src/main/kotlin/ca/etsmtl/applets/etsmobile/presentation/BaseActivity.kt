86b035b0a301ecc5bba2bf753a85888b2c7725e8
Thanh-Son-Philippe Lam <t.s.philippe_lam@hotmail.com>
2020-01-19 20:02:28 -0500

    Apply language preference

--- android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/presentation/BaseActivity.kt
+++ android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/presentation/BaseActivity.kt
@@ -1,8 +1,12 @@
 package ca.etsmtl.applets.etsmobile.presentation
 
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import ca.etsmtl.applets.etsmobile.R
 import dagger.android.support.DaggerAppCompatActivity
 import model.UserCredentials
+import java.util.Locale
 
 /**
  * This base activity saves the user's credentials when [onSaveInstanceState] is called and restore
@@ -29,5 +33,32 @@ abstract class BaseActivity : DaggerAppCompatActivity() {
         outState.putParcelable(STATE_SIGNETS_CREDENTIALS, UserCredentials.INSTANCE)
 
         super.onSaveInstanceState(outState)
+    }
+
+    override fun attachBaseContext(newBase: Context) {
+        val context = newBase.createLanguagePrefConfigurationContext()
+
+        super.attachBaseContext(context)
+    }
+
+    @Suppress("DEPRECATION")
+    private fun Context.createLanguagePrefConfigurationContext(): Context {
+        val prefsFileName = getString(R.string.key_preference_file)
+        val prefs = getSharedPreferences(prefsFileName, Context.MODE_PRIVATE)
+        val langPrefKey = getString(R.string.key_language_pref)
+        val lang = prefs.getString(langPrefKey, null) ?: getString(R.string.default_entry_value_language_pref)
+        val locale = Locale(lang, "CA")
+        Locale.setDefault(locale)
+        val overrideConfiguration = resources
+            .configuration
+            .apply {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    setLocale(locale)
+                } else {
+                    this.locale = locale
+                }
+            }
+
+        return createConfigurationContext(overrideConfiguration)
     }
 }
